d788b64dd842078905b8291e74b1862c6908c0f8
ProgProgrammer <progprogrammer@yandex.ru>
2019-05-01 16:27:27 +0400

    First_course_Lesson_1

---
+++ 2019/first_course/lesson1/1_option/js/script1.js
@@ -0,0 +1,3 @@
+var degree = prompt('Введите градусы по Цельсию, которые нужно перевести /n в градус ы по Фаренгейту');
+var fahrenheit = (9 / 5)*degree+32;
+alert(fahrenheit);
---
+++ 2019/first_course/lesson1/3_option/js/lesson12.js
@@ -0,0 +1,3 @@
+var number = "108";
+var summ = 1000 + number;
+alert(summ);
