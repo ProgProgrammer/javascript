f4ec90a008c830c706d006c0aebec84e18889740
ProgProgrammer <progprogrammer@yandex.ru>
2019-10-02 22:12:04 +0400

    Refactoring № 4.3.

--- 2019/tic tac toe/trial_code.js
+++ 2019/tic tac toe/trial_code.js
@@ -8,14 +8,19 @@ function getAllLines(matrix) {
     let lines = [];
     lines.push(getHorisontalLines(matrix));
     lines.push(getVerticalLines(matrix));
+    lines.push(getDiagonalLines(matrix));
 }
 
 function getHorisontalLines(matrix) {    
     for (let total_array = 0; total_array < matrix.length; total_array++) {
         let line = matrix[total_array];
+        
+        for (let internal_array = 0; internal_array < matrix.length; internal_array++) {
+            line += matrix[total_array][internal_array];
             
-        if(line === "XXX" || line === "OOO") {
-            return line;
+            if(total_array == 2) {
+                return line;
+            }
         }
     }
 }
@@ -26,9 +31,25 @@ function getVerticalLines(matrix) {
         for (let internal_array = 0; internal_array < matrix.length; internal_array++) {
             let line += matrix[internal_array][total_array];
             
-            if(line === "XXX" || line === "OOO") {
+            if(total_array == 2) {
                 return line;
             }
         }
     }
+}
+
+function getDiagonalLines(matrix) {
+    let line = [];
+    let countdown = 3;
+    
+    for (let total_array = 0; total_array < matrix.length; total_array++) {
+        line += matrix[total_array][total_array];
+    }
+    
+    for (let total_array = 0; total_array < matrix.length; total_array++) {
+        countdown -= 1;
+        line += matrix[total_array][countdown];
+    }
+    
+    return line;
 }
