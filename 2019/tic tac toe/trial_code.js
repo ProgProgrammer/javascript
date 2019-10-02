a4c9165845853a725adff333c750001c006149e5
ProgProgrammer <progprogrammer@yandex.ru>
2019-10-02 10:44:41 +0400

    Refactoring № 4.2.

--- 2019/tic tac toe/trial_code.js
+++ 2019/tic tac toe/trial_code.js
@@ -1,15 +1,30 @@
 let matrix = [
-            ['X', 'X', 'X'],
-            ['-', '-', '-'],
-            ['-', '-', '-']
+            'XXX',
+            '-',
+            '-'
         ];
 
-function getHorisontalLines(matrix) {
+function getAllLines(matrix) {
+    let lines = [];
+    lines.push(getHorisontalLines(matrix));
+    lines.push(getVerticalLines(matrix));
+}
+
+function getHorisontalLines(matrix) {    
     for (let total_array = 0; total_array < matrix.length; total_array++) {
-        let line = "";
+        let line = matrix[total_array];
+            
+        if(line === "XXX" || line === "OOO") {
+            return line;
+        }
+    }
+}
+
+function getVerticalLines(matrix) {    
+    for (let total_array = 0; total_array < matrix.length; total_array++) {
         
         for (let internal_array = 0; internal_array < matrix.length; internal_array++) {
-            line += matrix[total_array][internal_array];
+            let line += matrix[internal_array][total_array];
             
             if(line === "XXX" || line === "OOO") {
                 return line;
