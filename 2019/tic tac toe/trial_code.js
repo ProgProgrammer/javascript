406adf48c9fa2343d49cac04747ff045476661b3
ProgProgrammer <progprogrammer@yandex.ru>
2019-10-03 22:23:44 +0400

    Refactoring № 4.7.

--- 2019/tic tac toe/trial_code.js
+++ 2019/tic tac toe/trial_code.js
@@ -12,14 +12,15 @@ function getAllLines(matrix) {
     return lines;
 }
 
-function getHorisontalLines(matrix) {    
+function getHorisontalLines(matrix) {  
+    let line = ['', '', ''];
+    
     for (let total_array = 0; total_array < matrix.length; total_array++) {
-        let line = matrix[total_array];
         
         for (let internal_array = 0; internal_array < matrix.length; internal_array++) {
-            line += matrix[total_array][internal_array];
+            line[total_array] += matrix[total_array][internal_array];
             
-            if(total_array == matrix.length-1) {
+            if(total_array == matrix.length-1 && internal_array == matrix.length-1) {
                 return line;
             }
         }
@@ -27,12 +28,14 @@ function getHorisontalLines(matrix) {
 }
 
 function getVerticalLines(matrix) {    
+    let line = ['', '', ''];
+    
     for (let total_array = 0; total_array < matrix.length; total_array++) {
         
         for (let internal_array = 0; internal_array < matrix.length; internal_array++) {
-            let line += matrix[internal_array][total_array];
+            line[internal_array] += matrix[internal_array][total_array];
             
-            if(total_array == matrix.length-1) {
+            if(total_array == matrix.length-1 && internal_array == matrix.length-1) {
                 return line;
             }
         }
@@ -40,16 +43,16 @@ function getVerticalLines(matrix) {
 }
 
 function getDiagonalLines(matrix) {
-    let line = [];
+    let line = ['', ''];
     let countdown = matrix.length;
     
     for (let total_array = 0; total_array < matrix.length; total_array++) {
-        line += matrix[total_array][total_array];
+        line[0] += matrix[total_array][total_array];
     }
     
     for (let total_array = 0; total_array < matrix.length; total_array++) {
         countdown -= 1;
-        line += matrix[total_array][countdown];
+        line[1] += matrix[total_array][countdown];
     }
     
     return line;
