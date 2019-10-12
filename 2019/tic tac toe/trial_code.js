f2f308c20d052357d7b36c00bebcbc49a923c7c7
ProgProgrammer <progprogrammer@yandex.ru>
2019-10-12 17:37:24 +0400

    Refactoring № 5.1.

--- 2019/tic tac toe/trial_code.js
+++ 2019/tic tac toe/trial_code.js
@@ -10,20 +10,22 @@ function ticTacTooWin(matrix, count_clicks) {
     const lines = getAllLines(matrix);
     const isXWin = isSymbolWin(lines, "X");
     const isOWin = isSymbolWin(lines, "O");
-    let result = getResult(isXWin, isOWin, count_clicks);
+    let result = getResult(isXWin, isOWin, count_clicks, matrix);
     return result;
 }
 
-function getResult(isXWin, isOWin, count_clicks) {
+function getResult(isXWin, isOWin, count_clicks, matrix) {
     
     if (isXWin === "X") {
         const x = "X";
         return x;
     } else if (isOWin === "O") {
-        const o = 0;
+        const o = "O";
         return o;
     } else if (count_clicks === matrix.length * 3) {
         return "Ничья";
+    } else {
+        return;
     }
 }
 
@@ -31,31 +33,24 @@ function isSymbolWin(lines, symbol) {
     for (let line = 0; line < lines.length; line++) {
 
         for (let internal_line = 0; internal_line < lines.length; internal_line++) {
-            let countSymbolsX = 0;
-            let countSymbolsO = 0;
+            let countSymbols = 0;
 
             for (let internal_internal_line = 0; internal_internal_line < lines.length; internal_internal_line++) {
-
+                    
                 if (lines[line][internal_line][internal_internal_line] === symbol) {
 
-                    if (countSymbolsX === lines.length - 1) {
-                        const x = "X";
-                        return x;
+                    if (countSymbols === lines.length - 1) {
+                        const xo = symbol;
+                        return xo;
                     }
                     
-                    countSymbolsX += 1;
-                }
-
-                if (lines[line][internal_line][internal_internal_line] === symbol) {
-
-                    if (countSymbolsO === lines.length - 1) {
-                        const o = "O";
-                        return o;
-                    }
-                    
-                    countSymbolsO += 1;
+                    countSymbols += 1;
                 }
             }
+        }
+        
+        if (line === lines.length - 1) {
+            return;
         }
     }
 }
@@ -99,7 +94,7 @@ function getVerticalLines(matrix) {
 }
 
 function getDiagonalLines(matrix) {
-    let line = ['', ''];
+    let line = ['', '', ''];
     let countdown = matrix.length;
 
     for (let total_array = 0; total_array < matrix.length; total_array++) {
@@ -109,12 +104,13 @@ function getDiagonalLines(matrix) {
     for (let total_array = 0; total_array < matrix.length; total_array++) {
         countdown -= 1;
         line[1] += matrix[total_array][countdown];
+        line[2] += "-";
     }
 
     return line;
 }
 
-if(ticTacTooWin(["OOX", "OX-", "XOO"], count_clicks) === 'X' && ticTacTooWin(["XOX", "OX-", "OOO"], count_clicks) === 0  && ticTacTooWin(["XOX", "XO-", "XXO"], count_clicks) === "X" && ticTacTooWin(["XOX", "OX-", "XOO"], count_clicks) === "X" && ticTacTooWin(["XOX", "OX-", "OOX"], count_clicks) === "X" && ticTacTooWin(["OXO", "XO-", "XOO"], count_clicks) === 0 && ticTacTooWin(["OXO", "XO-", "OOX"], count_clicks) === 0 && ticTacTooWin(["---", "---", "---"], count_clicks) === undefined) {
+if(ticTacTooWin(["OOX", "OX-", "XOO"], count_clicks) === 'X' && ticTacTooWin(["XOX", "OX-", "OOO"], count_clicks) === "O"  && ticTacTooWin(["XOX", "XO-", "XXO"], count_clicks) === "X" && ticTacTooWin(["XOX", "OX-", "XOO"], count_clicks) === "X" && ticTacTooWin(["XOX", "OX-", "OOX"], count_clicks) === "X" && ticTacTooWin(["OXO", "XO-", "XOO"], count_clicks) === "O" && ticTacTooWin(["OXO", "XO-", "OOX"], count_clicks) === "O" && ticTacTooWin(["OXO", "XO-", "OOX"], 9) === "O" && ticTacTooWin(["OXO", "XOX", "XOX"], 9) === "Ничья") {
 	alert('OK');
 } else {
 	alert('NO OK');
