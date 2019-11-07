051d15b1903652d0f823e260c288fa99f39fd72d
ProgProgrammer <progprogrammer@yandex.ru>
2019-11-07 21:50:44 +0400

    Max-height: refactoring: reduce parameters.

--- 2019/Max-height/max-height.js
+++ 2019/Max-height/max-height.js
@@ -2,22 +2,22 @@ window.onload = function() {
     const blocks = document.getElementsByClassName("col-xs-4");
     const images = document.getElementsByTagName("img");
     const texts = document.getElementsByClassName("caption");
-    check_blocks(blocks, images, texts);
+    resizeBlocks(blocks, images, texts);
 }
 
-function check_blocks(blocks, images, texts) {
+function resizeBlocks(blocks, images, texts) {
     let contents = {};
     contents.blocks = blocks;
     contents.images = images;
     contents.texts = texts;
-    for (let content in contents) {
-        let max = contents[content][0].offsetHeight;
-        const blocks_height = check_height(max, contents[content]);
-        appropriation_styles(blocks_height, contents[content]);
+    for (let property in contents) {
+        const blocks_height = getMaxHeight(contents[property]);
+        appropriation_styles(blocks_height, contents[property]);
     }
 }
 
-function check_height(max, blocks) {
+function getMaxHeight(blocks) {
+    let max = blocks[0].offsetHeight;
 	for (let block = 0; block < blocks.length; block++) {
 		if(blocks[block].offsetHeight > max) {
     	   max = blocks[block].offsetHeight;
