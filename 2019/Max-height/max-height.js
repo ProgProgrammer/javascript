785ab0f449ef30a501be1e724e57aeb1acba14c4
ProgProgrammer <progprogrammer@yandex.ru>
2019-11-07 08:15:21 +0400

    Program for assigning maximum height to blocks.

---
+++ 2019/Max-height/max-height.js
@@ -0,0 +1,37 @@
+let contents = {};
+
+window.onload = function() {
+    const blocks = document.getElementsByClassName("col-xs-4");
+    const images = document.getElementsByTagName("img");
+    const texts = document.getElementsByClassName("caption");
+    check_blocks(blocks, images, texts);
+}
+
+function check_blocks(blocks, images, texts) {
+    contents.blocks = blocks;
+    contents.images = images;
+    contents.texts = texts;
+    for (let content in contents) {
+        let max = contents[content][0].offsetHeight;
+        const blocks_height = check_height(max, contents[content]);
+        appropriation_styles(blocks_height, contents[content]);
+    }
+}
+
+function check_height(max, blocks) {
+	for (let block = 0; block < blocks.length; block++) {
+		if(blocks[block].offsetHeight > max) {
+    	   max = blocks[block].offsetHeight;
+        }
+    
+        if(block == blocks.length - 1) {
+            return max;
+        }
+    }
+}
+
+function appropriation_styles(blocks_height, blocks) {
+    for (let i = 0; i < blocks.length; i++) {
+        blocks[i].style.height = blocks_height + "px";
+    }
+}
