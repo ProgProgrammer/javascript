51562eccb4298b87015c9da4f3e111e45ad7496a
ProgProgrammer <progprogrammer@yandex.ru>
2019-11-17 00:49:04 +0400

    Number sorting: refactoring: 1) function startFilter renamed startSorting; 2) added function custNumbers; 3) changed functions regularExpressions, maxNumber, minNumber, elementsFiltering.

--- 2019/Filter/filter.js
+++ 2019/Filter/filter.js
@@ -7,78 +7,85 @@ function main() {
         clicks += 1;
         const blocks = document.getElementsByClassName("block");
         const texts = document.getElementsByClassName("block-p");
-        startFilter(clicks, button, blocks, texts);
+        startSorting(clicks, button, blocks, texts);
     });
 
     button_summ.addEventListener('click', function () {
         clicks_summ += 1;
         const blocks = document.getElementsByClassName("block-summ");
         const texts = document.getElementsByClassName("block-p-summ");
-        startFilter(clicks_summ, button_summ, blocks, texts);
+        startSorting(clicks_summ, button_summ, blocks, texts);
     });
 }
 
-function startFilter(clicks, button, blocks, texts) {
-    const id_elements = [];
-    const min_number = minNumber(texts, button);
-    const max_number = maxNumber(texts, button);
-    allNumbers(texts, max_number, min_number, id_elements, button);
-    elementsFiltering(id_elements, blocks, clicks, button);
-}
-
-function minNumber(texts, button) {
-    let min_number = regularExpressions(texts, 0, button);
-    min_number = Number(min_number);
-    for (let text = 0; text < texts.length; text++) {
-        const text_replace = regularExpressions(texts, text, button);
-        if (text_replace < min_number) {
-            min_number = Number(text_replace);
-        }
+function startSorting(clicks, button, blocks, texts) {
+    let id_elements = [];
+    custNumbers(blocks, id_elements, button);
+    const copy_id_elements = [];
+    for (let i = 0; i < id_elements.length; i++) {
+        copy_id_elements[i] = id_elements[i];
     }
-    return min_number;
-}
-
-function maxNumber(texts, button) {
-    let max_number = regularExpressions(texts, 0, button);
-    max_number = Number(max_number);
-    for (let text = 0; text < texts.length; text++) {
-        const text_replace = regularExpressions(texts, text, button);
-        if (text_replace > max_number) {
-            max_number = Number(text_replace);
-        }
+    const id_order_array = [];
+    const numbers_array = [];
+    const max_number = maxNumber(id_elements, copy_id_elements, numbers_array);
+    for (let element = 0; element < id_elements.length; element++) {
+        minNumber(copy_id_elements, numbers_array, element, max_number);
     }
-    return max_number;
+    elementsFiltering(id_elements, numbers_array, blocks, clicks, button);
 }
 
-function allNumbers(texts, max_number, min_number, id_elements, button) {
-    let counter_id = -1;
-    for (let number = min_number; number <= max_number; number++) {
-        for (let element_array = 0; element_array < texts.length; element_array++) {
-            let text = regularExpressions(texts, element_array, button);
-            if (Number(text) == number) {
-                counter_id += 1;
-                id_elements[counter_id] = element_array;
-            }
-        }
+function custNumbers(blocks, id_elements, button) {
+    for (let i = 0; i < blocks.length; i++) {
+        let number = regularExpressions(blocks, i, button);
+        id_elements[i] = number;
     }
 }
 
 function regularExpressions(blocks, blocks_element, button) {
     if (button.classList.contains("button")) {
-        return blocks[blocks_element].innerHTML.replace(/от\s/g, "").replace(/,\d+%/g, "");
+        const number = blocks[blocks_element].textContent.replace(/от\s/g, "").replace(/,\d+%/g, "");
+        return Number(number);
     } else if (button.classList.contains("button-summ")) {
-        return blocks[blocks_element].innerHTML.replace(/\s/g, "").replace(/.{3}$/, "");
+        const number = blocks[blocks_element].textContent.replace(/\s/g, "").replace(/,/g, "");
+        return Number(number);
     }
 }
 
-function elementsFiltering(id_elements, blocks, clicks, button) {
-    for (let id_element = 0; id_element < id_elements.length; id_element++) {
-        if (clicks % 2) {
-            blocks[id_elements[id_element]].style.order = id_element;
-            button.classList.remove("arrow-table");
-        } else {
-            blocks[id_elements[id_element]].style.order = id_elements.length - id_element;
-            button.classList.add("arrow-table");
+function maxNumber(id_elements, copy_id_elements, numbers_array) {
+    let max_number = id_elements[0];
+    for (let i = 0; i < copy_id_elements.length; i++) {
+        if (copy_id_elements[i] > max_number) {
+            max_number = copy_id_elements[i];
+        }
+    }
+    return max_number;
+}
+
+function minNumber(copy_id_elements, numbers_array, element, max_number) {
+    let min_number = max_number;
+    const id_array = [];
+    for (let i = 0; i < copy_id_elements.length; i++) {
+        if (copy_id_elements[i] < min_number) {
+            min_number = copy_id_elements[i];
+            id_array[0] = i;
+        }
+    }
+    delete copy_id_elements[id_array[0]];
+    numbers_array[element] = min_number;
+}
+
+function elementsFiltering(id_elements, numbers_array, blocks, clicks, button) {
+    for (let numbers_array_element = 0; numbers_array_element < numbers_array.length; numbers_array_element++) {
+        for (let id_element = 0; id_element < id_elements.length; id_element++) {
+            if (numbers_array[numbers_array_element] == id_elements[id_element]) {
+                if (clicks % 2) {
+                    blocks[id_element].style.order = numbers_array_element;
+                    button.classList.remove("arrow-table");
+                } else {
+                    blocks[id_element].style.order = numbers_array.length - numbers_array_element;
+                    button.classList.add("arrow-table");
+                }
+            }
         }
     }
 }
