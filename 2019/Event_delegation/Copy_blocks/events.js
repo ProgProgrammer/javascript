0129df0d6510ca093b74d3a591a542d2b4b01d5e
ProgProgrammer <progprogrammer@yandex.ru>
2019-11-27 22:49:26 +0400

    Copy_blocks: Added remove blocks.

--- 2019/Event_delegation/Copy_blocks/events.js
+++ 2019/Event_delegation/Copy_blocks/events.js
@@ -4,8 +4,13 @@ accordions.addEventListener("click", function (event) {
     const blocks = accordions.getElementsByClassName("blocks");
     const target = event.target;
     console.log(target.classList.contains("block-center"));
-    if (target.classList.contains("block-center")) {
-        const clone_block = blocks[0].cloneNode(true);
-        accordions.append(clone_block);
+    if (target.classList.contains("block-center") || target.parentNode.classList.contains("block-center")) {
+        if (target.classList.contains("copy") || target.parentNode.classList.contains("copy")) {
+            const clone_block = blocks[0].cloneNode(true);
+            accordions.append(clone_block);
+        } else if (target.classList.contains("delete") || target.parentNode.classList.contains("delete")) {
+            const block = target.closest(".blocks");
+            block.remove();
+        }
     }
 });
