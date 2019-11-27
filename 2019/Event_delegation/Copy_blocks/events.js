9167c3d9f9ec845b47db27435da66ee4c136ecec
ProgProgrammer <progprogrammer@yandex.ru>
2019-11-27 22:54:49 +0400

    Copy_blocks: remove console.log().

--- 2019/Event_delegation/Copy_blocks/events.js
+++ 2019/Event_delegation/Copy_blocks/events.js
@@ -3,7 +3,6 @@ const accordions = document.querySelector(".accrodions");
 accordions.addEventListener("click", function (event) {
     const blocks = accordions.getElementsByClassName("blocks");
     const target = event.target;
-    console.log(target.classList.contains("block-center"));
     if (target.classList.contains("block-center") || target.parentNode.classList.contains("block-center")) {
         if (target.classList.contains("copy") || target.parentNode.classList.contains("copy")) {
             const clone_block = blocks[0].cloneNode(true);
