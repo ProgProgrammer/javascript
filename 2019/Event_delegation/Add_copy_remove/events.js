6ab88581c44a5ff7d814b8f4d50d962cd2a8708b
ProgProgrammer <progprogrammer@yandex.ru>
2019-11-28 23:57:36 +0400

    Add_copy_remove: added function add block.

---
+++ 2019/Event_delegation/Add_copy_remove/events.js
@@ -0,0 +1,28 @@
+window.addEventListener("load", main);
+
+function main() {
+    const input_text = document.querySelector(".main-top-input");
+    const button = document.querySelector(".main-top-button");
+    const main_blocks = document.querySelector(".main-blocks");
+
+    button.addEventListener('click', function () {
+        addText(input_text, button, main_blocks);
+    });
+}
+
+function addText(input_text, button, main_blocks) {
+    console.log(input_text.value == "");
+    if (input_text.value == "") {
+        input_text.classList.add("color-red");
+    } else if (input_text.value != "") {
+        const block = main_blocks.querySelector(".main-blocks-block");
+        if (input_text.classList.contains("color-red")) {
+            input_text.classList.remove("color-red");
+        }
+        const clone_block = block.cloneNode(true);
+        main_blocks.append(clone_block);
+        const text = clone_block.querySelector(".main-blocks-block-line-p");
+        text.textContent = input_text.value;
+        clone_block.classList.remove("display-none");
+    }
+}
