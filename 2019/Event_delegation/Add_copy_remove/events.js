cc8c33d2843bad097ad68f7919e8c7978ad30244
ProgProgrammer <progprogrammer@yandex.ru>
2019-12-02 01:55:52 +0400

    Add_copy_remove: added object "main_object", function copyBlockCopy.

--- 2019/Event_delegation/Add_copy_remove/events.js
+++ 2019/Event_delegation/Add_copy_remove/events.js
@@ -1,70 +1,77 @@
 window.addEventListener("load", main);
 
 function main() {
-    const input_text = document.querySelector(".main-top-input");
-    const button = document.querySelector(".main-top-button");
-    const main_blocks = document.querySelector(".main-blocks");
-    let clicks = -1;
+    let main_object = {};
+    main_object.input_text = document.querySelector(".main-top-input");
+    main_object.button = document.querySelector(".main-top-button");
+    main_object.main_blocks = document.querySelector(".main-blocks");
+    main_object.clicks = 0;
+    main_object.clicks_add = 1;
 
-    button.addEventListener('click', function () {
-        addText(input_text, button, main_blocks);
+    main_object.button.addEventListener('click', function () {
+        //main_object.clicks_add++;
+        addText(main_object);
     });
 
-    main_blocks.addEventListener('click', function (event) {
-        const target = event.target;
-        if (target.classList.contains("main-blocks-block-copy")) {
-            clicks++;
-            clicks = clickCounter(main_blocks, target, clicks);
-            copyBlock(main_blocks, target, clicks);
-        } else if (target.classList.contains("main-blocks-block-remove")) {
-            removeBlock(main_blocks, target);
+    main_object.main_blocks.addEventListener('click', function (event) {
+        main_object.target = event.target;
+        if (main_object.target.classList.contains("main-blocks-block-copy")) {
+            main_object.clicks++;
+            console.log(main_object.clicks);
+            main_object.clicks = clickCounter(main_object);
+            console.log(main_object.clicks);
+            copyBlock(main_object);
+        } else if (main_object.target.classList.contains("main-blocks-block-remove")) {
+            removeBlock(main_object);
         }
     });
 }
 
-function addText(input_text, button, main_blocks) {
-    if (input_text.value == "") {
-        input_text.classList.add("color-red");
-    } else if (input_text.value != "") {
-        const block = main_blocks.querySelector(".main-blocks-block");
-        if (input_text.classList.contains("color-red")) {
-            input_text.classList.remove("color-red");
+function addText(main_object) {
+    if (main_object.input_text.value == "") {
+        main_object.input_text.classList.add("color-red");
+    } else if (main_object.input_text.value != "") {
+        const block = main_object.main_blocks.querySelector(".main-blocks-block");
+        if (main_object.input_text.classList.contains("color-red")) {
+            main_object.input_text.classList.remove("color-red");
         }
         const clone_block = block.cloneNode(true);
-        main_blocks.append(clone_block);
+        main_object.main_blocks.append(clone_block);
         const text = clone_block.querySelector(".main-blocks-block-line-p-span");
-        text.textContent = input_text.value;
+        text.textContent = main_object.input_text.value;
         clone_block.classList.remove("display-none");
     }
 }
 
-function clickCounter(main_blocks, target, clicks) {
-    const blocks = main_blocks.getElementsByClassName("main-blocks-block");
+function clickCounter(main_object) {
+    const blocks = main_object.main_blocks.getElementsByClassName("main-blocks-block");
     for (let i = 0; i < blocks.length; i++) {
-        if (blocks[i].querySelector(".main-blocks-block-copy") == target) {
+        if (blocks[i].querySelector(".main-blocks-block-copy") == main_object.target) {
             let text_block = blocks[i].querySelector(".main-blocks-block-line-p-span-copy");
+            console.log(text_block);
             text_block = text_block.textContent;
             text_block = text_block.replace(/- /g, "").replace(/copy/g, "").replace(/[(]/g, "").replace(/[)]/g, "");
             text_block == Number(text_block);
             let text_block_span = blocks[i].querySelector(".main-blocks-block-line-p-span");
             text_block_span = text_block_span.textContent;
             const max_number = maxNumber(blocks, text_block, text_block_span);
+            console.log(max_number);
             if (text_block < Number(max_number)) {
                 return Number(max_number) + 1;
             } else if (text_block == Number(max_number)) {
                 return Number(max_number) + 1;
             } else {
-                return clicks;
+                return main_object.clicks;
             }
         }
     }
 }
 
 function maxNumber(blocks, number, text_block_span) {
-    console.log(text_block_span);
     for (let i = 0; i < blocks.length; i++) {
         let text = blocks[i].querySelector(".main-blocks-block-line-p-span");
         text = text.textContent;
+        console.log(text);
         if (text == text_block_span) {
             let text_block = blocks[i].querySelector(".main-blocks-block-line-p-span-copy");
             text_block = text_block.textContent;
@@ -74,22 +81,28 @@ function maxNumber(blocks, number, text_block_span) {
             }
         }
     }
+    console.log(number);
     return number;
 }
 
-function copyBlock(main_blocks, target, clicks) {
-    const block = target.closest(".main-blocks-block");
+function copyBlock(main_object) {
+    const block = main_object.target.closest(".main-blocks-block");
     const clone_block = block.cloneNode(true);
-    main_blocks.append(clone_block);
-    const text = clone_block.querySelector(".main-blocks-block-line-p-span-copy");
-    if (clicks >= 1) {
-        text.textContent = "- copy(" + clicks + ")";
+    main_object.main_blocks.append(clone_block);
+    main_object.text = clone_block.querySelector(".main-blocks-block-line-p-span-copy");
+    main_object.text_content = main_object.text.textContent
+    copyBlockCopy(main_object);
+}
+
+function copyBlockCopy(main_object) {
+    if (main_object.clicks >= 1 && main_object.text_content.indexOf("copy")) {
+        main_object.text.textContent = "- copy(" + main_object.clicks + ")";
     } else {
-        text.textContent = "- copy";
+        main_object.text.textContent = "- copy";
     }
 }
 
-function removeBlock(main_blocks, target) {
-    const block = target.closest(".main-blocks-block");
+function removeBlock(main_object) {
+    const block = main_object.target.closest(".main-blocks-block");
     block.remove();
 }
