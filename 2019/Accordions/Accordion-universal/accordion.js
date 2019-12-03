4b9a4c92b8b0f1c0b0de9a7e7329e305a1162c59
ProgProgrammer <progprogrammer@yandex.ru>
2019-12-04 00:54:21 +0400

    Accordion-universal: added several accordions.

--- 2019/Accordions/Accordion-universal/accordion.js
+++ 2019/Accordions/Accordion-universal/accordion.js
@@ -3,17 +3,24 @@ window.onresize = main;
 
 function main() {
     const main_object = {};
-    main_object.buttons = document.getElementsByClassName("accrodion-button");
-    main_object.accordion_hidden = document.getElementsByClassName("accordion-blocks-block");
-    main_object.accordion_content = document.getElementsByClassName("accordion-blocks-block-contents");
-    main_object.accordion_block = document.querySelector(".block-center");
-    main_object.speed = main_object.accordion_block.dataset.speed;
+    main_object.accordion = document.querySelector(".accordions");
+    main_object.accordion_blocks = document.getElementsByClassName("block-center");
 
-    for (let i = 0; i < main_object.buttons.length; i++) {
-        main_object.buttons[i].addEventListener('click', function () {
-            if (main_object.accordion_block.classList.contains("closing-accordion") === false && main_object.accordion_block.classList.contains("opening-accordion") === false) {
-                main_object.i = i;
-                openCloseAccordion(main_object);
+    for (let x = 0; x < main_object.accordion_blocks.length; x++) {
+        main_object.accordion_blocks[x].addEventListener('click', function (event) {
+            if (main_object.accordion.classList.contains("closing-accordion") === false && main_object.accordion.classList.contains("opening-accordion") === false) {
+                main_object.accordion_block = main_object.accordion_blocks[x];
+                let target = event.target;
+                main_object.buttons = main_object.accordion_block.getElementsByClassName("accrodion-button");
+                main_object.accordion_hidden = main_object.accordion_block.getElementsByClassName("accordion-blocks-block");
+                main_object.accordion_content = main_object.accordion_block.getElementsByClassName("accordion-blocks-block-contents");
+                main_object.speed = main_object.accordion_block.dataset.speed;
+                for (let i = 0; i < main_object.buttons.length; i++) {
+                    if (main_object.buttons[i] == target) {
+                        main_object.i = i;
+                        openCloseAccordion(main_object);
+                    }
+                }
             }
         });
     }
@@ -29,14 +36,13 @@ function openCloseAccordion(main_object) {
         }
     }
 
-    console.log(main_object.accordion_hidden[main_object.i].offsetHeight);
     if (main_object.accordion_hidden[main_object.i].offsetHeight === 0) {
         openAccordion(main_object);
     }
 }
 
 function openAccordion(main_object) {
-    main_object.accordion_block.classList.add("opening-accordion");
+    main_object.accordion.classList.add("opening-accordion");
     const content_height = main_object.accordion_content[main_object.i].offsetHeight;
     main_object.accordion_hidden[main_object.i].style.transitionDuration = main_object.speed + "s";
     main_object.accordion_hidden[main_object.i].style.height = content_height + "px";
@@ -44,14 +50,14 @@ function openAccordion(main_object) {
     setTimeout(endAccordion, delay);
 
     function endAccordion() {
-        main_object.accordion_block.classList.remove("opening-accordion");
+        main_object.accordion.classList.remove("opening-accordion");
         main_object.accordion_block.classList.add('open-accordion');
         main_object.accordion_hidden[main_object.i].style.height = 'auto';
     }
 }
 
 function closeAccordion(main_object) {
-    main_object.accordion_block.classList.add("closing-accordion");
+    main_object.accordion.classList.add("closing-accordion");
     const content_height = main_object.accordion_content[main_object.i].offsetHeight;
     main_object.accordion_hidden[main_object.a].style.transitionDuration = main_object.speed + "s";
     main_object.accordion_hidden[main_object.a].style.height = content_height + "px";
@@ -67,6 +73,6 @@ function closeAccordion(main_object) {
     }
 
     function removeClass() {
-        main_object.accordion_block.classList.remove("closing-accordion");
+        main_object.accordion.classList.remove("closing-accordion");
     }
 }
