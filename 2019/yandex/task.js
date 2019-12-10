f9c7ae2f391fd233bf94296f5cb6eea45654bc78
ProgProgrammer <progprogrammer@yandex.ru>
2019-12-10 23:15:44 +0400

    Yandex task: refactoring program.

--- 2019/yandex/task.js
+++ 2019/yandex/task.js
@@ -1,80 +1,43 @@
-function OneEditApart(...args) {
-    const main_object = {};
-    main_object.array_main = [[], []];
-    main_object.array_numbers = [];
-    main_object.i = [];
-    main_object.dissimilarity = 0;
-    main_object.not_similarity = 0;
-    main_object.string_check = "";
+function OneEditApart(string_left, string_right) {
+    let long_String = "";
+    let short_String = "";
 
-    for (let i = 0; i < args.length; i++) {
-        for (let a = 0; a < args[i].length; a++) {
-            main_object.array_main[i][a] = args[i][a];
-        }
-    }
-
-    if (main_object.array_main[0].length >= main_object.array_main[1].length) {
-        main_object.max_i = 0;
-        main_object.min_i = 1;
-
-    } else if (main_object.array_main[0].length < main_object.array_main[1].length) {
-        main_object.max_i = 1;
-        main_object.min_i = 0;
-    }
-
-    for (let i = 0; i < main_object.array_main[main_object.min_i].length; i++) {
-        for (let a = 0; a < main_object.array_main[main_object.max_i].length; a++) {
-            if (main_object.array_main[main_object.min_i][i] === main_object.array_main[main_object.max_i][a]) {
-                main_object.not_similarity++;
-                main_object.i[i] = a;
-            }
-        }
-    }
-
-    if (main_object.not_similarity <= main_object.array_main[main_object.max_i].length - 2) {
-        main_object.not_similarity = 0;
-        return false;
-    }
-
-    main_object.not_similarity = 0;
-
-    for (let i = 0; i < main_object.array_main[main_object.max_i].length; i++) {
-        if (i !== main_object.i[i] && main_object.dissimilarity === 0) {
-            main_object.dissimilarity++;
-            main_object.string = main_object.array_main[main_object.max_i][i];
-            main_object.index = i;
-        }
-    }
-
-    if (main_object.max_i === 1) {
-        for (let a = main_object.array_main[main_object.max_i].length - 1; a >= main_object.index; a--) {
-            if (a === main_object.index) {
-                main_object.array_main[main_object.min_i][a] = main_object.string;
-
-            } else {
-                main_object.array_main[main_object.min_i][a] = main_object.array_main[main_object.max_i][a];
-            }
-
-        }
-
-        for (let i = 0; i < main_object.array_main[main_object.min_i].length; i++) {
-            main_object.string_check += main_object.array_main[main_object.min_i][i];
-        }
-
-    } else if (main_object.max_i === 0) {
-        main_object.array_main[main_object.max_i].splice(main_object.index, 1);
-
-        for (let i = 0; i < main_object.array_main[main_object.max_i].length; i++) {
-            main_object.string_check += main_object.array_main[main_object.max_i][i];
-        }
-    }
-
-    if (main_object.string_check === args[1]) {
-        return true;
+    if (string_left.length > string_right.length) {
+        long_String = string_left;
+        short_String = string_right;
     } else {
+        long_String = string_right;
+        short_String = string_left;
+    }
+
+    if (long_String.length - short_String.length >= 2) {
         return false;
     }
 
+    let count = 0;
+    let i = 0;
+    let a = 0;
+
+    while (i < long_String.length && a < short_String.length) {
+        if (long_String[i] === short_String[a]) {
+            i++;
+            a++;
+            continue;
+        } else {
+            count++;
+            if (count > 1) {
+                return false;
+            }
+
+            if (long_String.length != short_String.length) {
+                i++;
+            } else {
+                i++;
+                a++;
+            }
+        }
+    }
+    return true;
 }
 
 console.log(OneEditApart("cat", "dog"));
