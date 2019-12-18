fc0c427e085ba59386349e1ffd959c82b7368f67
ProgProgrammer <progprogrammer@yandex.ru>
2019-12-19 01:09:57 +0400

    Guess the number: inputs validation improved.

--- 2019/Tasks/task01/task.js
+++ 2019/Tasks/task01/task.js
@@ -9,47 +9,25 @@ function main() {
     let number_Round = 0;
     const object = {};
     object.number_Made = false;
+    object.input_NoValidate = false;
+    const number_Min = numbers[0];
+    const number_Max = numbers[1];
+
+    for (let i = 0; i < numbers.length - 1; i++) {
+        numbers[i].oninput = function () {
+            console.log(i);
+            if (checkInputs(numbers, object, input_Window, number_Min, number_Max, "part", i) === false) {
+                return;
+            }
+        }
+    }
 
     button_Enter.addEventListener('click', () => {
-        const number_Min = numbers[0];
-        const number_Max = numbers[1];
-        input_Window[0].style.display = "none";
-        input_Window[1].style.display = "none";
-
-        for (let i = 0; i < numbers.length - 1; i++) {
-
-            if (numbers[i].value == "") {
-                numbers[i].style.borderColor = "red";
-            }
-
-            if (numbers[i].value != "" && numbers[i].style.borderColor == "red") {
-                numbers[i].style.borderColor = "black";
-            }
-
-            if (numbers[i].value > 100 || numbers[i].value < 0) {
-                numbers[i].style.borderColor = "red";
-                input_Window[i].innerText = "Допускаются числа от 0 до 100";
-                input_Window[i].style.display = "flex";
-            } else if (numbers[i].value >= 0 && numbers[i].value <= 100 && input_Window[i].style.display == "flex") {
-                numbers[i].style.borderColor = "black";
-                input_Window[i].style.display = "none";
-            }
-        }
-
-        if (number_Min.value != "" && number_Max.value != "" &&
-            Number(number_Min.value) > Number(number_Max.value)) {
-            number_Min.style.borderColor = "red";
-            number_Max.style.borderColor = "red";
-            input_Window[0].innerText = "Число больше максимального";
-            input_Window[1].innerText = "Число меньше минимального";
-            input_Window[0].style.display = "flex";
-            input_Window[1].style.display = "flex";
-        }
-
-        if (number_Min.style.borderColor == "red" || number_Max.style.borderColor == "red" || numbers[2].style.borderColor == "red") {
+        if (checkInputs(numbers, object, input_Window, number_Min, number_Max, "all") === false) {
+            console.log(checkInputs(numbers, input_Window, number_Min, number_Max, "all") === false);
             return;
         }
-
+        console.log("hello");
         if (object.number_Made === false) {
             let number_Random = number_Min.value - 0.5 + Math.random() * (number_Max.value - number_Min.value + 1);
             number_Round = Math.round(number_Random);
@@ -65,6 +43,8 @@ function main() {
         } else {
             numbers[3].setAttribute("value", "Угадал");
             object.number_Made = false;
+            number_Round = 0;
+            resetInputs(numbers, input_Window, 2);
             return;
         }
     });
@@ -72,9 +52,77 @@ function main() {
     button_Reset.addEventListener('click', () => {
         object.number_Made = false;
         number_Round = 0;
-        numbers[0].value = "";
-        numbers[1].value = "";
-        numbers[2].value = "";
-        numbers[3].setAttribute("value", "");
+        resetInputs(numbers, input_Window, 1);
+        return;
     });
+}
+
+function checkInputs(numbers, object, input_Window, number_Min, number_Max, word, i) {
+    for (let i = 0; i < numbers.length - 1; i++) {
+        if (word == "all") {
+            console.log(i);
+            if (numbers[i].value == "") {
+                numbers[i].style.borderColor = "red";
+                object.input_NoValidate = true;
+            }
+            if (i == numbers.length - 2 && object.input_NoValidate === true) {
+                console.log("false");
+                object.input_NoValidate = false;
+                return false;
+            }
+        }
+    }
+    console.log(i);
+    if (i !== undefined) {
+        console.log("undefined");
+        if (numbers[i].value < 0 || numbers[i].value > 100) {
+            numbers[i].style.borderColor = "red";
+            input_Window[i].innerText = "Допускаются числа от 0 до 100";
+            input_Window[i].style.display = "flex";
+            if (i == 0 && input_Window[1].innerText == "Число меньше минимального") {
+                numbers[1].style.borderColor = "black";
+                input_Window[1].style.display = "none";
+            } else if (i == 1 && input_Window[0].innerText == "Число больше максимального") {
+                numbers[0].style.borderColor = "black";
+                input_Window[0].style.display = "none";
+            }
+            return false;
+        } else if (numbers[i].value >= 0 && numbers[i].value <= 100 &&
+            numbers[i].style.borderColor == "red") {
+            console.log(numbers[i].value);
+            numbers[i].style.borderColor = "black";
+            input_Window[i].style.display = "none";
+        }
+    }
+
+
+    if (Number(number_Min.value) > Number(number_Max.value) &&
+        number_Min.value != "" && number_Max.value != "") {
+        number_Min.style.borderColor = "red";
+        number_Max.style.borderColor = "red";
+        input_Window[0].style.display = "flex";
+        input_Window[1].style.display = "flex";
+        input_Window[0].innerText = "Число больше максимального";
+        input_Window[1].innerText = "Число меньше минимального";
+        return false;
+    } else if (Number(number_Min.value) <= Number(number_Max.value) &&
+        input_Window[0].innerText != "Допускаются числа от 0 до 100" &&
+        input_Window[1].innerText != "Допускаются числа от 0 до 100") {
+        number_Min.style.borderColor = "black";
+        number_Max.style.borderColor = "black";
+        input_Window[0].style.display = "none";
+        input_Window[1].style.display = "none";
+    }
+}
+
+function resetInputs(numbers, input_Window, a) {
+    for (let i = 0; i < numbers.length - a; i++) {
+        numbers[i].value = "";
+        numbers[i].style.borderColor = "black";
+        input_Window[i].style.display = "none";
+    }
+
+    if (a == 1) {
+        numbers[numbers.length - a].setAttribute("value", "");
+    }
 }
