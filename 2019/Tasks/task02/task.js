3d2674fe65aa90fc49fd5a9fabc8dc28abd09160
ProgProgrammer <progprogrammer@yandex.ru>
2019-12-22 01:10:36 +0400

    Text Replacement.

---
+++ 2019/Tasks/task02/task.js
@@ -0,0 +1,8 @@
+main();
+
+function main() {
+    const groups_Buttons = document.querySelectorAll(".group-buttons");
+    const input_Text = document.querySelector(".input");
+    const texts = document.querySelectorAll(".form-texts-text");
+    const object_Buttons = {};
+}
