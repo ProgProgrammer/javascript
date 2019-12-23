b4d11cb5b85b99541946ffe78aee97b46e54f281
ProgProgrammer <progprogrammer@yandex.ru>
2019-12-23 23:06:53 +0400

    Text Replacement: program logic has been changed: only one of two groups of buttons can be pressed.

--- 2019/Tasks/task02/task.js
+++ 2019/Tasks/task02/task.js
@@ -6,7 +6,7 @@ function main() {
     const texts = document.querySelectorAll(".form-texts-text");
     const button_Style = document.querySelector(".button-style");
     const window_Absolute = document.querySelector(".form-column-input-text");
-    const window_Absolute_Text = window_Absolute.querySelectorAll(".form-column-input-text-p");
+    const window_Absolute_Text = window_Absolute.querySelector(".form-column-input-text-p");
     const object_Buttons = {};
     object_Buttons.buttons_Top = "";
     object_Buttons.buttons_Bottom = "";
@@ -16,8 +16,15 @@ function main() {
             const buttons = groups_Buttons[i].querySelectorAll(".button");
             for (let a = 0; a < buttons.length; a++) {
                 if (buttons[a] == event.target) {
-                    buttons[a].style.background = "red";
-                    checkPartButtons(object_Buttons, groups_Buttons, i, buttons, a, window_Absolute_Text, input_Text);
+                    if (checkPartButtons(object_Buttons, groups_Buttons, i, buttons, a, window_Absolute_Text, input_Text) === false) {
+                        return;
+                    } else {
+                        buttons[a].style.background = "red";
+                        borderNone();
+                        groups_Buttons[i].classList.add("background-red");
+                        console.log(object_Buttons.buttons_Top);
+                        console.log(object_Buttons.buttons_Bottom);
+                    }
                 }
             }
         });
@@ -40,84 +47,89 @@ function main() {
 }
 
 function checkAllButtons(object_Buttons, groups_Buttons, window_Absolute, window_Absolute_Text, input_Text) {
-    const buttons = document.querySelectorAll(".button");
-    for (let i = 0; i < buttons.length; i++) {
-        if (buttons[i].style.borderColor == "red") {
-            buttons[i].style.borderColor == "black";
+
+    if (object_Buttons.buttons_Top == "" && object_Buttons.buttons_Bottom == "") {
+
+        if (object_Buttons.buttons_Top == "") {
+            const buttons_Group_Top = document.querySelector(".form-column");
+            buttons_Group_Top.classList.add("border-red");
+            window_Absolute.style.display = "flex";
+            window_Absolute_Text.style.display = "flex";
+            input_Text.style.borderColor = "red";
         }
-    }
 
-    if (object_Buttons.buttons_Top == "") {
-        const buttons_Group_Top = document.querySelector(".form-column");
-        buttons_Group_Top.classList.add("border-red");
-        window_Absolute.style.display = "flex";
-        window_Absolute_Text[0].style.display = "flex";
-        input_Text.style.borderColor = "red";
+        if (object_Buttons.buttons_Bottom == "") {
+            const buttons_Group_Bottom = document.querySelector(".form-buttons-input");
+            buttons_Group_Bottom.classList.add("border-red");
+            window_Absolute.style.display = "flex";
+            window_Absolute_Text.style.display = "flex";
+            input_Text.style.borderColor = "red";
+        }
+    } else {
+        borderNone();
     }
-
-    if (object_Buttons.buttons_Bottom == "") {
-        const buttons_Group_Bottom = document.querySelector(".form-buttons-input");
-        buttons_Group_Bottom.classList.add("border-red");
-        window_Absolute.style.display = "flex";
-        window_Absolute_Text[1].style.display = "flex";
-        input_Text.style.borderColor = "red";
-    }
-
-    /*if (input_Text.value == "") {
-        window_Absolute.style.display = "none";
-        window_Absolute_Text[0].style.display = "none";
-        window_Absolute_Text[1].style.display = "none";
-    }*/
 }
 
 function checkPartButtons(object_Buttons, groups_Buttons, i, buttons, a, window_Absolute_Text, input_Text) {
-    if (groups_Buttons[i].classList.contains("border-red")) {
-        groups_Buttons[i].classList.remove("border-red");
-    }
-    for (let b = 0; b < buttons.length; b++) {
-        if (buttons[b].style.background == "red" && b !== a) {
-            buttons[b].style.background = "buttonface";
+    if (groups_Buttons[0].classList.contains("background-red") && i == 1) {
+        console.log("hello");
+        return false;
+    } else if (groups_Buttons[1].classList.contains("background-red") && i == 0) {
+        console.log("good");
+        return false;
+    } else {
+        for (let c = 0; c < groups_Buttons.length; c++) {
+            if (groups_Buttons[c].classList.contains("border-red")) {
+                groups_Buttons[c].classList.remove("border-red");
+            }
         }
-    }
-    if (i == 0) {
-        if (a == 0) {
-            object_Buttons.buttons_Top = "insert-tags";
-        } else if (a == 1) {
-            object_Buttons.buttons_Top = "insert-no-tags";
-        } else if (a == 2) {
-            object_Buttons.buttons_Top = "replacement-tags";
-        } else if (a == 3) {
-            object_Buttons.buttons_Top = "replacement-no-tags";
+        for (let b = 0; b < buttons.length; b++) {
+            if (buttons[b].style.background == "red" && b !== a) {
+                buttons[b].style.background = "buttonface";
+            }
         }
+        if (i == 0) {
+            if (a == 0) {
+                object_Buttons.buttons_Top = "insert-tags";
+            } else if (a == 1) {
+                object_Buttons.buttons_Top = "insert-no-tags";
+            } else if (a == 2) {
+                object_Buttons.buttons_Top = "replacement-tags";
+            } else if (a == 3) {
+                object_Buttons.buttons_Top = "replacement-no-tags";
+            }
 
-        checkWindowAbsolute(i, window_Absolute_Text, input_Text);
-    } else if (i == 1) {
-        if (a == 0) {
-            object_Buttons.buttons_Bottom = "before-begin";
-        } else if (a == 1) {
-            object_Buttons.buttons_Bottom = "after-begin";
-        } else if (a == 2) {
-            object_Buttons.buttons_Bottom = "before-end";
-        } else if (a == 3) {
-            object_Buttons.buttons_Bottom = "after-end";
-        }
+            checkWindowAbsolute(i, window_Absolute_Text, input_Text);
+        } else if (i == 1) {
+            if (a == 0) {
+                object_Buttons.buttons_Bottom = "before-begin";
+            } else if (a == 1) {
+                object_Buttons.buttons_Bottom = "after-begin";
+            } else if (a == 2) {
+                object_Buttons.buttons_Bottom = "before-end";
+            } else if (a == 3) {
+                object_Buttons.buttons_Bottom = "after-end";
+            }
 
-        checkWindowAbsolute(i, window_Absolute_Text, input_Text);
+            checkWindowAbsolute(i, window_Absolute_Text, input_Text);
+        }
     }
 }
 
 function checkWindowAbsolute(i, window_Absolute_Text, input_Text) {
-    if (window_Absolute_Text[0].style.display == "flex" &&
-        window_Absolute_Text[1].style.display == "none" || window_Absolute_Text[1].style.display == "") {
+    if (window_Absolute_Text.style.display == "flex") {
         document.querySelector(".form-column-input-text").style.display = "none";
-        window_Absolute_Text[0].style.display = "none";
+        window_Absolute_Text.style.display = "none";
         input_Text.style.borderColor = "black";
-    } else if (window_Absolute_Text[1].style.display == "flex" &&
-        window_Absolute_Text[0].style.display == "none" || window_Absolute_Text[0].style.display == "") {
-        document.querySelector(".form-column-input-text").style.display = "none";
-        window_Absolute_Text[1].style.display = "none";
-        input_Text.style.borderColor = "black";
-    } else if (window_Absolute_Text[i].style.display == "flex") {
-        window_Absolute_Text[i].style.display = "none";
+    }
+}
+
+function borderNone() {
+    const buttons = document.querySelectorAll(".button");
+    for (let i = 0; i < buttons.length; i++) {
+        if (buttons[i].style.borderColor == "red") {
+            buttons[i].style.borderColor == "black";
+            console.log(i);
+        }
     }
 }
