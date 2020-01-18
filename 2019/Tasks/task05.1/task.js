ed9f5f7d60ba3f580000c6856a1fe73213289673
ProgProgrammer <progprogrammer@yandex.ru>
2020-01-18 20:56:34 +0400

    Object movement: added functions "checkDistance", "checkDistanceBottomRight"; changed function "movingObject".

--- 2019/Tasks/task05.1/task.js
+++ 2019/Tasks/task05.1/task.js
@@ -4,6 +4,8 @@ function main() {
     const windowBlock = document.querySelector(".window");
     const block = document.querySelector(".block");
     const marginBlock = 10;
+    const blockWidth = block.offsetWidth;
+    const blockHeight = block.offsetHeight;
     const obj = {};
     obj.top = 'KeyW';
     obj.left = 'KeyA';
@@ -11,73 +13,128 @@ function main() {
     obj.bottom = 'KeyS';
     
     document.onkeypress = (e) => {
-        //block.style.position = "absolute";
         obj.keypress = e.code;
-        movingObject(e, block, marginBlock, obj, windowBlock);
+        const windowWidth = windowBlock.clientWidth;
+        const windowHeight = windowBlock.clientHeight;
+        return movingObject(e, block, marginBlock, obj, windowWidth, windowHeight, blockWidth, blockHeight);
     }
 }
 
-function movingObject(e, block, marginBlock, obj, windowBlock) {
-    const windowWidth = windowBlock.clientWidth;
-    const windowHeight = windowBlock.clientHeight;
+function movingObject(e, block, marginBlock, obj, windowWidth, windowHeight, blockWidth, blockHeight) {
+    let resultFunction;
     if (e.code === obj.top || e.code === obj.bottom)
     {
         switch(e.code)
         {
             case obj.top:
-                const resultFunction = checkDistance();
+                resultFunction = checkDistance(marginBlock, block, block.offsetTop, windowHeight, blockHeight);
                 switch(resultFunction)
                 {
                     case true:
                         block.style.top = block.offsetTop - marginBlock + "px";
+                        block.style.position = "absolute";
                         break;
 
                     default:
                         block.style.top = block.offsetTop - resultFunction + "px";
-                }  
+                        block.style.position = "absolute";
+                        break;
+                } 
+                break;
             
             case obj.bottom:
-                const resultFunction = checkDistance();
+                resultFunction = checkDistance(marginBlock, block, "bottom", windowHeight, blockHeight);
                 switch(resultFunction)
                 {
                     case true:
                         block.style.top = block.offsetTop + marginBlock + "px";
+                        block.style.position = "absolute";
                         break;
 
                     default:
                         block.style.top = block.offsetTop + resultFunction + "px";
+                        block.style.position = "absolute";
+                        break;
                 }
+                break;
         }
     }
     else if (e.code === obj.left || e.code === obj.right)
     {
-        const resultFunction = checkDistance();
         switch(e.code)
         {
             case obj.left:
-                const resultFunction = checkDistance();
+                resultFunction = checkDistance(marginBlock, block, block.offsetLeft, windowWidth, blockWidth);
                 switch(resultFunction)
                 {
                     case true:
-                        block.style.top = block.offsetTop - marginBlock + "px";
+                        block.style.left = block.offsetLeft - marginBlock + "px";
+                        block.style.position = "absolute";
                         break;
 
                     default:
-                        block.style.top = block.offsetTop - resultFunction + "px";
-                }  
+                        block.style.left = block.offsetLeft - resultFunction + "px";
+                        block.style.position = "absolute";
+                        break;
+                } 
+                break;
             
             case obj.right:
-                const resultFunction = checkDistance();
+                resultFunction = checkDistance(marginBlock, block, "right", windowWidth, blockWidth);
                 switch(resultFunction)
                 {
                     case true:
-                        block.style.top = block.offsetTop + marginBlock + "px";
+                        block.style.left = block.offsetLeft + marginBlock + "px";
+                        block.style.position = "absolute";
                         break;
 
                     default:
-                        block.style.top = block.offsetTop + resultFunction + "px";
+                        block.style.left = block.offsetLeft + resultFunction + "px";
+                        block.style.position = "absolute";
+                        break;
                 }
+                break;
         }
+    }
+}
+
+function checkDistance(marginBlock, block, blockOffset, windowSize, blockSize) {
+    if (blockOffset === "bottom" || blockOffset === "right")
+    {
+        blockOffset = checkDistanceBottomRight(marginBlock, block, blockOffset, windowSize, blockSize);
+    }
+    
+    if (blockOffset >= marginBlock)
+    {
+        return true;
+    }
+    else if (windowSize - (blockOffset + blockSize) < marginBlock)
+    {
+        const result = windowSize - (blockOffset + blockSize);
+        return result;
+    }
+    else if (blockOffset < marginBlock)
+    {
+        return blockOffset;
+    }
+}
+
+function checkDistanceBottomRight(marginBlock, block, blockOffset, windowSize, blockSize) {
+    let result;
+    /*console.log(windowSize);
+    console.log(block.offsetTop);
+    console.log(blockSize);*/
+    if (blockOffset === "bottom")
+    {
+        result = windowSize - (block.offsetTop + blockSize);
+        console.log(result);
+        return result;
+    }
+    else if (blockOffset === "right")
+    {
+        result = windowSize - (block.offsetLeft + blockSize);
+        console.log(result);
+        return result;
     }
 }
 
