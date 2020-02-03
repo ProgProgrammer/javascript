2a2eebd884de5267ff794e6c51df161a3ea8a458
ProgProgrammer <progprogrammer@yandex.ru>
2020-02-04 02:10:05 +0400

    Slider: added functions "widthHeightWindow", "createSliders", "timingSlides".

--- 2019/Tasks/task07/task.js
+++ 2019/Tasks/task07/task.js
@@ -1,37 +1,75 @@
 (function () {
     let windowSlider;
-    let slide;
     let timing;
     let arrows;
-    let windowWidth;
-    let windowHeight;
+    let obj = {};
+    obj.windowWidth;
+    obj.windowHeight;
+    obj.slides;
     
     window.addEventListener('DOMContentLoaded', ()=> {
         windowSlider = document.querySelector(".window");
-        slide = document.querySelector(".window-blocks");
-        timing = slide.dataset.timing;
+        obj.slides = document.querySelectorAll(".window-blocks");
+        timing = windowSlider.dataset.timing;
         arrows = document.querySelectorAll(".arrow");
-        windowWidth = windowSlider.offsetWidth;
-        slide.style.width = windowWidth + "px";
-        windowHeight = windowSlider.offsetHeight;
-        slide.style.height = windowHeight + "px";
-        heightArrows(arrows, windowHeight);
+        obj.windowWidth = windowSlider.offsetWidth;
+        obj.windowHeight = windowSlider.offsetHeight;
+        widthHeightWindow(obj.windowWidth, obj.windowHeight);
+        heightArrows(obj.windowHeight);
+        createSliders(obj.windowWidth);
         
         window.addEventListener('resize', ()=> {
-            windowWidth = windowSlider.offsetWidth;
-            slide.style.width = windowWidth + "px";
-            windowHeight = windowSlider.offsetHeight;
-            slide.style.height = windowHeight + "px";
-            heightArrows(arrows, windowHeight);
+            obj.windowWidth = windowSlider.offsetWidth;
+            obj.windowHeight = windowSlider.offsetHeight;
+            obj.slides = document.querySelectorAll(".window-blocks");
+            widthHeightWindow(obj.windowWidth, obj.windowHeight);
+            heightArrows(obj.windowHeight);
         });
         
-        function heightArrows(arrows, windowHeight) {
+        function widthHeightWindow(windowWidth, windowHeight) {
+            for (let i = 0; i < obj.slides.length; i++)
+            {
+                obj.slides[i].style.width = windowWidth + "px";
+                obj.slides[i].style.height = windowHeight + "px";
+            }            
+        }
+        
+        function heightArrows(windowHeight) {
             let partHeight;
             
             for(let i = 0; i < arrows.length; i++)
             {
                 partHeight = arrows[i].offsetHeight;
                 arrows[i].style.top = (windowHeight/2 - partHeight/2) + "px";
+            }
+        }
+        
+        function createSliders(windowWidth) {  
+            for (let a = 0; a < 2; a++)
+            {
+                windowSlider.insertAdjacentHTML("afterbegin", obj.slides[0].outerHTML);
+            }
+            const slides = document.querySelectorAll(".window-blocks");
+            slides[0].style.transform = "translate3d(-" + windowWidth + "px, 0px, 0px)";
+            slides[2].style.transform = "translate3d(" + windowWidth + "px, 0px, 0px)";
+            
+            setTimeout(timingSlides, timing, slides);
+        }
+        
+        function timingSlides(slides) {
+            if (timing !== "")
+            {
+                for (let i = 0; i < slides.length; i++)
+                {
+                    slides[i].style.transitionDuration = timing + "ms";
+                }
+            }   
+            else
+            {
+                for (let i = 0; i < slides.length; i++)
+                {
+                    slides[i].style.transitionDuration = 500 + "ms";
+                }
             }
         }
     });
