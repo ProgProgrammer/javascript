82b56359b902fba963603512bd34a3b44ceebeca
CS <zubr.free@yandex.ru>
2020-02-22 22:01:55 +0400

    Slider: added "blockBullets" variable and "paginationMoving" function.

--- 2019/Tasks/task07/libraryMobileSlider.js
+++ 2019/Tasks/task07/libraryMobileSlider.js
@@ -3,11 +3,21 @@ export const mobileSliderObject = {};
 mobileSliderObject.mobileSlider = (i, mobileBlock, staticBlocks, timing, mainBlock, obj) =>
 {
     let htmlTags;
+    let blockBullets;
     const trafficSlider = mainBlock.dataset.trafficSlider;
+
+    if (mainBlock.dataset === "yes")
+    {
+        blockBullets = document.querySelectorAll(".window-block-pagination-block-bullet");
+    }
 
     if (trafficSlider === "true")
     {
         mainBlock.dataset.trafficSlider = false;
+        if (mainBlock.dataset.pagination === "yes")
+        {
+            mobileSliderObject.paginationMoving(i, obj);
+        }
     }
     else
     {
@@ -42,20 +52,55 @@ mobileSliderObject.mobileSlider = (i, mobileBlock, staticBlocks, timing, mainBlo
         setTimeout(mobileSliderObject.deleteTag, timing, staticBlocks, i, mobileBlock, mainBlock, obj);
         if (obj.widthTransform > 0 && (/-/.test(mobileBlock.style.transform) === false))
         {
-            console.log(obj.widthTransform);
+            //console.log(obj.widthTransform);
             obj.widthTransform -= staticBlocks[0].offsetWidth;
-            console.log(obj.widthTransform);
+            //console.log(obj.widthTransform);
             mobileBlock.style.left = "-" + (obj.widthTransform + staticBlocks[0].offsetWidth) + "px";
             mobileBlock.style.transform = "translate3d(" + obj.widthTransform + "px, 0px, 0px)";
         }
         else
         {
-            console.log(mobileBlock.style.transform);
-            console.log(obj.widthTransform);
+            //console.log(mobileBlock.style.transform);
+            //console.log(obj.widthTransform);
             obj.widthTransform += staticBlocks[0].offsetWidth;
-            console.log(obj.widthTransform);
+            //console.log(obj.widthTransform);
             mobileBlock.style.left = (obj.widthTransform - staticBlocks[0].offsetWidth) + "px";
             mobileBlock.style.transform = "translate3d(-" + obj.widthTransform + "px, 0px, 0px)";
+        }
+    }
+}
+
+mobileSliderObject.paginationMoving = (i, obj) =>
+{
+    const bullets = document.querySelectorAll(".window-block-pagination-block-bullet");
+    let bulletId = obj.paginationBullet;
+    bullets[bulletId].style.backgroundColor = "grey";
+    console.log(i);
+
+    if (i === 0)
+    {
+        if (bulletId > 0)
+        {
+            bullets[bulletId - 1].style.backgroundColor = "white";
+            obj.paginationBullet = bulletId - 1;
+        }
+        else
+        {
+            bullets[bullets.length - 1].style.backgroundColor = "white";
+            obj.paginationBullet = bullets.length - 1;
+        }
+    }
+    else if (i === 1)
+    {
+        if (bulletId < bullets.length - 1)
+        {
+            bullets[bulletId + 1].style.backgroundColor = "white";
+            obj.paginationBullet = bulletId + 1;
+        }
+        else
+        {
+            bullets[0].style.backgroundColor = "white";
+            obj.paginationBullet = 0;
         }
     }
 }
@@ -72,24 +117,24 @@ mobileSliderObject.deleteTag = (staticBlocks, i, mobileBlock, mainBlock, obj) =>
     else if (obj.staticBlocksLength === 1 && i === 1 && staticBlocks[2] !== undefined && staticBlocks!== null)
     {
         staticBlocks[2].remove();
-        console.log(i);
+        //console.log(i);
     }
     else if (obj.staticBlocksLength > 1)
     {
         staticBlocks[0].remove();
-        console.log(i);
-        console.log(mobileBlock.style.left);
-        console.log(/-/.test(mobileBlock.style.left));
+        //console.log(i);
+        //console.log(mobileBlock.style.left);
+        //console.log(/-/.test(mobileBlock.style.left));
 
         if (/-/.test(mobileBlock.style.left) === true)
         {
             mobileBlock.style.left = "-" + (obj.widthTransform + staticBlocks[0].offsetWidth) + "px";
-            console.log(mobileBlock.style.left);
+            //console.log(mobileBlock.style.left);
         }
         else
         {
             mobileBlock.style.left = (obj.widthTransform + staticBlocks[0].offsetWidth) + "px";
-            console.log(mobileBlock.style.left);
+            //console.log(mobileBlock.style.left);
         }
     }
     else
