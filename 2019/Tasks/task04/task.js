50ce32e40c95035c9bf8548f807df9e326f3bf47
ProgProgrammer <progr.programmer@yandex.ru>
2020-02-25 00:17:27 +0400

    Switch: code refactoring: code made more readable.

--- 2019/Tasks/task04/task.js
+++ 2019/Tasks/task04/task.js
@@ -1,35 +1,40 @@
-window.onload = main;
+(function()
+{
+    window.onload = main;
 
-function main() {
-    let tab = document.querySelectorAll(".tab");
+    function main() {
+        let tab = document.querySelectorAll(".tab");
 
-    init();
+        init();
 
-    function init() {
-        let tabBody = document.querySelectorAll(".tab-body");
-        const number = 1;
-        cycle(number, tabBody);
-    }
+        function init() {
+            let tabBody = document.querySelectorAll(".tab-body");
+            const number = 1;
+            cycle(number, tabBody);
+        }
 
-    tab.forEach((element) => {
-        element.onclick = showTabs;
-    });
+        tab.forEach((element) => {
+            element.onclick = showTabs;
+        });
 
-    function showTabs() {
-        const data = this.getAttribute("data");
-        const tabBody = document.querySelectorAll(".tab-body");
-        const number = 0;
-        cycle(number, tabBody, data);
-        document.querySelector(`.tab-body[data="${data}"]`).style.display = "block";
-        document.querySelector(`.tab[data="${data}"]`).classList.add("active");
-    }
+        function showTabs() {
+            const data = this.getAttribute("data");
+            const tabBody = document.querySelectorAll(".tab-body");
+            const number = 0;
+            cycle(number, tabBody, data);
+            document.querySelector(`.tab-body[data="${data}"]`).style.display = "block";
+            document.querySelector(`.tab[data="${data}"]`).classList.add("active");
+        }
 
-    function cycle(number, tabBody, data) {
-        for (let i = number; i < tabBody.length; i++) {
-            tabBody[i].style.display = "none";
-            if (number != 1) {
-                tab[i].classList.remove("active");
+        function cycle(number, tabBody, data) {
+            for (let i = number; i < tabBody.length; i++) {
+                tabBody[i].style.display = "none";
+                if (number != 1) {
+                    tab[i].classList.remove("active");
+                }
             }
         }
     }
-}
+})
+
+()
