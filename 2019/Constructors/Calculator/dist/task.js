952a4c169c60e6e392797477ba85882fb0158f5c
ProgProgrammer <progr.programmer@yandex.ru>
2020-03-21 19:07:29 +0400

    Calculator: added "Calculator" function in task.js file, with "inputValue" and "calculateValue" functions; added functions of addition and subtraction.

--- 2019/Constructors/Calculator/dist/task.js
+++ 2019/Constructors/Calculator/dist/task.js
@@ -119,7 +119,84 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   return newRequire;
 })({"task.js":[function(require,module,exports) {
 (function () {
-  console.log("Hello");
+  var calculate;
+  var buttons;
+  var buttonValue;
+
+  function Calculator() {
+    this.numbersExpression = 0;
+    this.arraySymbols = [];
+    this.input = document.querySelector(".main-blocks-input");
+
+    this.inputValue = function (symbol) {
+      this.symbol = symbol;
+      this.input.value = this.input.value.replace(/ /gi, "");
+
+      if (this.symbol !== "=") {
+        console.log(/\d/gi.test(this.symbol));
+
+        if (this.arraySymbols.length === 0 || /\d/gi.test(this.symbol) === false || /\d/gi.test(this.arraySymbols[this.arraySymbols.length - 1]) === false) {
+          this.arraySymbols.push(this.symbol);
+          this.input.value += this.symbol;
+          console.log(this.arraySymbols);
+        } else {
+          this.arraySymbols[this.arraySymbols.length - 1] += this.symbol;
+          this.input.value += this.symbol;
+          console.log(this.arraySymbols);
+        }
+      } else {
+        this.input.value += this.symbol;
+        console.log(this.symbol);
+      }
+    };
+
+    this.calculateValue = function () {
+      console.log(this.arraySymbols);
+
+      for (var a = 0; a < this.arraySymbols.length; a++) {
+        //console.log(Number(this.arraySymbols[a]));
+        if (/\d/gi.test(this.arraySymbols[a]) === false && /\d/gi.test(this.arraySymbols[a + 1]) === true) {
+          if (this.arraySymbols[a] === "+") {
+            this.numbersExpression = this.numbersExpression + Number(this.arraySymbols[a + 1]);
+            console.log(this.numbersExpression);
+          } else if (this.arraySymbols[a] === "-") {
+            this.numbersExpression = this.numbersExpression - Number(this.arraySymbols[a + 1]);
+            console.log(this.numbersExpression);
+          }
+        } else if (/\d/gi.test(this.arraySymbols[a]) === true) {
+          if (this.numbersExpression === 0) {
+            this.numbersExpression = Number(this.arraySymbols[a]);
+            console.log(this.numbersExpression);
+          }
+        }
+      }
+
+      console.log(this.numbersExpression);
+      this.input.value += this.numbersExpression;
+    };
+  }
+
+  window.addEventListener('DOMContentLoaded', function () {
+    console.log("Hello");
+    buttons = document.querySelectorAll(".main-blocks-buttons-button");
+    calculate = new Calculator();
+
+    var _loop = function _loop(i) {
+      buttons[i].addEventListener('click', function () {
+        buttonValue = buttons[i].dataset.name;
+        console.log(buttonValue);
+        calculate.inputValue(buttonValue);
+
+        if (buttonValue === "=") {
+          calculate.calculateValue();
+        }
+      });
+    };
+
+    for (var i = 0; i < buttons.length; i++) {
+      _loop(i);
+    }
+  });
 })();
 },{}],"../../../../../../../../AppData/Roaming/npm/node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
--- 2019/Constructors/Calculator/js/task.js
+++ 2019/Constructors/Calculator/js/task.js
@@ -1,6 +1,97 @@
 (function()
 {
-  console.log("Hello");
+  let calculate;
+  let buttons;
+  let buttonValue;
+
+  function Calculator()
+  {
+    this.numbersExpression = 0;
+    this.arraySymbols = [];
+    this.input = document.querySelector(".main-blocks-input");
+
+     this.inputValue = function(symbol)
+     {
+          this.symbol = symbol;
+          this.input.value = this.input.value.replace(/ /gi, "");
+          if (this.symbol !== "=")
+          {
+              console.log(/\d/gi.test(this.symbol));
+              if (this.arraySymbols.length === 0 ||
+                  /\d/gi.test(this.symbol) === false ||
+                  /\d/gi.test(this.arraySymbols[this.arraySymbols.length-1]) === false)
+              {
+                  this.arraySymbols.push(this.symbol);
+                  this.input.value += this.symbol;
+                  console.log(this.arraySymbols);
+              }
+              else
+              {
+                  this.arraySymbols[this.arraySymbols.length - 1] += this.symbol;
+                  this.input.value += this.symbol;
+                  console.log(this.arraySymbols);
+              }
+          }
+          else
+          {
+              this.input.value += this.symbol;
+              console.log(this.symbol);
+          }
+     }
+
+      this.calculateValue = function()
+      {
+          console.log(this.arraySymbols);
+          for (let a = 0; a < this.arraySymbols.length; a++)
+          {
+              //console.log(Number(this.arraySymbols[a]));
+              if (/\d/gi.test(this.arraySymbols[a]) === false && /\d/gi.test(this.arraySymbols[a + 1]) === true)
+              {
+                  if (this.arraySymbols[a] === "+")
+                  {
+                      this.numbersExpression = this.numbersExpression + Number(this.arraySymbols[a + 1]);
+                      console.log(this.numbersExpression);
+                  }
+                  else if (this.arraySymbols[a] === "-")
+                  {
+                      this.numbersExpression = this.numbersExpression - Number(this.arraySymbols[a + 1]);
+                      console.log(this.numbersExpression);
+                  }
+              }
+              else if (/\d/gi.test(this.arraySymbols[a]) === true)
+              {
+                  if (this.numbersExpression === 0)
+                  {
+                      this.numbersExpression = Number(this.arraySymbols[a]);
+                      console.log(this.numbersExpression);
+                  }
+              }
+          }
+          console.log(this.numbersExpression);
+          this.input.value += this.numbersExpression;
+      }
+  }
+
+  window.addEventListener('DOMContentLoaded', () =>
+  {
+      console.log("Hello");
+      buttons = document.querySelectorAll(".main-blocks-buttons-button");
+      calculate = new Calculator();
+
+      for (let i = 0; i < buttons.length; i++)
+      {
+          buttons[i].addEventListener('click', () =>
+          {
+              buttonValue = buttons[i].dataset.name;
+              console.log(buttonValue);
+              calculate.inputValue(buttonValue);
+              if (buttonValue === "=")
+              {
+                  calculate.calculateValue();
+              }
+          });
+      }
+  });
 })
 
 ()
