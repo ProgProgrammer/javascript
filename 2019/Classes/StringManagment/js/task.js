29f65ec307c3be25c36978518fd26df456365633
ProgProgrammer <progr.programmer@yandex.ru>
2020-05-04 16:28:22 +0400

    StringManagment: added variables in "task.js" file; added "click" event in "task.js" file.

--- 2019/Classes/StringManagment/js/task.js
+++ 2019/Classes/StringManagment/js/task.js
@@ -6,11 +6,30 @@ import { StringManagment } from "./libraryStringManagment.js";
     const objectStringManagment = {};
     objectStringManagment.buttons;
     objectStringManagment.tabs;
+    let addButton;
+    objectStringManagment.input;
+    objectStringManagment.addBlock;
+    objectStringManagment.stringsEditor;
+    objectStringManagment.stringsEditorText;
+    objectStringManagment.stringsPreview;
+    objectStringManagment.stringsPreviewText;
+    objectStringManagment.stringsPreviewDate;
+    objectStringManagment.stringsPreviewTime;
 
     window.addEventListener('DOMContentLoaded', () =>
     {
         objectStringManagment.buttons = document.querySelectorAll(".button");
         objectStringManagment.tabs = document.querySelectorAll(".tab");
+        addButton = document.querySelector(".preview-adding-button");
+        objectStringManagment.input = document.querySelector(".preview-adding-input");
+        objectStringManagment.columnEditor = document.querySelector(".preview-column");
+        objectStringManagment.editorBlock = document.querySelector(".preview-block");
+        objectStringManagment.stringsEditorText = objectStringManagment.editorBlock.querySelector(".text");
+        objectStringManagment.columnPreview = document.querySelector(".editor-column");
+        objectStringManagment.previewBlock = document.querySelector(".editor-block");
+        objectStringManagment.stringsPreviewText = objectStringManagment.previewBlock.querySelector(".text");
+        objectStringManagment.stringsPreviewDate = document.querySelector(".date");
+        objectStringManagment.stringsPreviewTime = document.querySelector(".time");
 
         stringManagment = new StringManagment(objectStringManagment);
 
@@ -21,6 +40,11 @@ import { StringManagment } from "./libraryStringManagment.js";
                 stringManagment.tabSwitching(i);
             });
         }
+
+        addButton.addEventListener("click", () =>
+        {
+            stringManagment.addString(objectStringManagment.editorBlock, objectStringManagment.previewBlock);
+        });
     });
 })
 
