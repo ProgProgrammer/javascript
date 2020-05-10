e7cc68959a0b3a91a852efe56a44438a411f1f90
ProgProgrammer <progr.programmer@yandex.ru>
2020-05-10 19:30:23 +0400

    InputForm: added "index.html" file with tags; added "style.css" file with styles; added "task.js" file; added "FormSubmition" class with "checkForm" and "formProcessing" methodes in "task.js" file; added "formSubmition" variable, "objectForm" object in "task.js" file; "text", "form", "formInput" and "formInputText" object properties in "task.js" file; added "submit" event in "task.js" file.

---
+++ 2019/AJAX/InputForm/task.js
@@ -0,0 +1,69 @@
+class FormSubmition
+{
+    constructor(object)
+    {
+        this.text = object.text;
+        this.formInput = object.formInput;
+        this.borderColorError;
+        this.borderColor;
+        this.textInput;
+        this.xhr;
+    }
+
+    checkForm(colorError, color)
+    {
+        this.borderColorError = colorError;
+        this.borderColor = color;
+
+        if (this.formInput.style.borderColor === this.borderColorError)
+        {
+            this.formInput.style.borderColor = this.borderColor;
+        }
+    }
+
+    formProcessing(textInput)
+    {
+        this.textInput = textInput;
+        this.textInput = encodeURIComponent(this.textInput);
+        this.xhr = new XMLHttpRequest();
+        this.xhr.open("POST", "form.php");
+        this.xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        this.xhr.send("form_input=" + this.textInput);
+    }
+}
+
+(function()
+{
+    let formSubmition;
+    objectForm = {};
+    objectForm.text;
+    objectForm.form;
+    objectForm.formInput;
+    objectForm.formInputText;
+
+    window.addEventListener("DOMContentLoaded", () =>
+    {
+        objectForm.text = document.querySelector(".text");
+        objectForm.form = document.forms.form;
+        objectForm.formInput = objectForm.form.form_input;
+
+        formSubmition = new FormSubmition(objectForm);
+
+        objectForm.form.onsubmit = (event) =>
+        {
+            event.preventDefault();
+            formSubmition.checkForm("red", "black");
+            if (objectForm.formInput.value !== "")
+            {
+                objectForm.formInputText = objectForm.formInput.value;
+                formSubmition.formProcessing(objectForm.formInputText);
+            }
+            else
+            {
+                objectForm.formInput.style.borderColor = "red";
+            }
+        }
+    });
+})
+
+()
