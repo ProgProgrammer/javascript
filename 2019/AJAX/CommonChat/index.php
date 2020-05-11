8284dd06bc22f67f77b1af642d4fb7173ad143db
ProgProgrammer <progr.programmer@yandex.ru>
2020-05-12 00:38:07 +0400

    CommonChat: added "meta name", "row" and "row-window" tags in "index.php" file; added styles in "style.css" file.

--- 2019/AJAX/CommonChat/index.php
+++ 2019/AJAX/CommonChat/index.php
@@ -3,17 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <title>Input form</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <div class="container">
-        <div class="text" id="answer"></div>
-        <form class="form" name="form">
-            <input class="input" type="text" name="form_input" placeholder="Введите текст">
-            <button class="button" type="submit" name="form_button">
-                Отправить
-            </button>
-        </form>
+        <div class="row">
+            <div class="row-window">
+                <div class="text" id="answer"></div>
+            </div>
+            <form class="form" name="form">
+                <input class="input" type="text" name="form_input" placeholder="Введите текст">
+                <button class="button" type="submit" name="form_button">
+                    Отправить
+                </button>
+            </form>
+        </div>
     </div>
     <script src="dist/task.js"></script>
 </body>
