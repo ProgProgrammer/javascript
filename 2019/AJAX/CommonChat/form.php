1cb95c889653819ce4add30320a7144a4de661ac
ProgProgrammer <progr.programmer@yandex.ru>
2020-06-01 22:03:43 +0400

    CommonChat: added "array" variable in "form.php" file; added "json_decode" method in "form.php" file.

--- 2019/AJAX/CommonChat/form.php
+++ 2019/AJAX/CommonChat/form.php
@@ -1,15 +1,17 @@
 <?php
 
+$array;
 $input = "";
 $textFile;
 $textArray = [];
 $textLines;
 $arraySize = 100;
 
-
-if ($_POST['form_input'] !== undefined && $_POST['form_input'] !== "")
+header("Content-Type: application/json; charset=UTF-8");
+$array = json_decode($_POST['form_input'], true);
+if ($array[0] !== undefined && $array[0] !== "")
 {
-    $input = htmlentities($_POST['form_input'], ENT_QUOTES, 'UTF-8');
+    $input = htmlentities($array[0], ENT_QUOTES, 'UTF-8');
     $input .= "\r\n";
     $textFile = fopen("text.txt", "a");
     fwrite($textFile, $input);
