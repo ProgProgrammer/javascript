e0dbbbea90e82a7c5b5b69dcc998389512c18b82
ProgProgrammer <progr.programmer@yandex.ru>
2020-07-26 12:49:13 +0400

    AdvertisingLandingPage: added "index.php" file.

---
+++ 2019/Tests/test03/index.php
@@ -0,0 +1,176 @@
+<!doctype html>
+<html lang="zxx">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=yes, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>POTENCIALEX</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/swiper-bundle.min.css">
+    <link rel="stylesheet" href="css/swiper.css">
+    <link rel="stylesheet" href="style.css?v=3">
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+</head>
+<body>
+<div id="anchors" class="common-block">
+    <div class="common-block-page">
+        <header class="header">
+            <section class="container">
+                <div class="row header-section">
+                    <h1 class="col-12 h1">
+                        potencialex
+                    </h1>
+                    <iframe id="ytplayer" src="https://www.youtube.com/embed/rUWxSEwctFU?autoplay=1" frameborder="0" allow="autoplay; fullscreen" allowfullscreen class="video"></iframe>
+                    <div class="col-12 block-button">
+                        <a href="#form" class="button down">Order <img src="img/arrow_down.svg" alt="arrow"></a>
+                    </div>
+                </div>
+            </section>
+        </header>
+        <main class="main">
+            <div class="container">
+                <div class="row display-flex-none">
+                    <div class="col-12">
+                        <div class="swiper-container slider">
+                            <div class="swiper-wrapper">
+                                <div class="swiper-slide" style="background-image: url(img/slide1.jpg);">
+                                    <div class="slide-shadow">
+                                        <p class="slide-text class-for-script">
+                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, amet culpa doloremque earum incidunt minus molestiae suscipit tempore temporibus! Assumenda at&nbsp;doloremque ex&nbsp;exercitationem numquam optio quae sapiente soluta suscipit.
+                                        </p>
+                                        <p class="slide-text class-for-script">
+                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, amet culpa doloremque earum incidunt minus molestiae suscipit tempore temporibus! Assumenda at&nbsp;doloremque ex&nbsp;exercitationem numquam optio quae sapiente soluta suscipit.
+                                        </p>
+                                        <p class="slide-text class-for-script">
+                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, amet culpa doloremque earum incidunt minus molestiae suscipit tempore temporibus! Assumenda at&nbsp;doloremque ex&nbsp;exercitationem numquam optio quae sapiente soluta suscipit.
+                                        </p>
+                                    </div>
+                                </div>
+                                <div class="swiper-slide" style="background-image: url(img/slide2.jpg);">
+                                    <div class="slide-shadow">
+                                        <p class="slide-text class-for-script">
+                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, amet culpa doloremque earum incidunt minus molestiae suscipit tempore temporibus! Assumenda at&nbsp;doloremque ex&nbsp;exercitationem numquam optio quae sapiente soluta suscipit.
+                                        </p>
+                                    </div>
+                                </div>
+                                <div class="swiper-slide" style="background-image: url(img/slide3.jpg);">
+                                    <div class="slide-shadow">
+                                        <p class="slide-text class-for-script">
+                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, amet culpa doloremque earum incidunt minus molestiae suscipit tempore temporibus! Assumenda at&nbsp;doloremque ex&nbsp;exercitationem numquam optio quae sapiente soluta suscipit.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Add Pagination -->
+                            <div class="swiper-pagination"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <section class="properties-product">
+                <div class="container">
+                    <h2 class="h2">
+                        product properties
+                    </h2>
+                    <div class="row properties-product-property">
+                        <div class="col-12 col-sm-6 col-md-4 properties-product-product">
+                            <p class="properties-product-text">
+                                Powerful
+                            </p>
+                            <p class="properties-product-text-property">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, amet culpa doloremque earum incidunt minus molestiae suscipit tempore temporibus! Assumenda at&nbsp;doloremque ex&nbsp;exercitationem numquam optio quae sapiente soluta suscipit.
+                            </p>
+                        </div>
+                        <div class="col-12 col-sm-6 col-md-4 properties-product-product">
+                            <p class="properties-product-text">
+                                Effective
+                            </p>
+                            <p class="properties-product-text-property">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, amet culpa doloremque earum incidunt minus molestiae suscipit tempore temporibus! Assumenda at&nbsp;doloremque ex&nbsp;exercitationem numquam optio quae sapiente soluta suscipit.
+                            </p>
+                        </div>
+                        <div class="col-12 col-sm-6 col-md-4 properties-product-product">
+                            <p class="properties-product-text">
+                                Fast acting
+                            </p>
+                            <p class="properties-product-text-property">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, amet culpa doloremque earum incidunt minus molestiae suscipit tempore temporibus! Assumenda at&nbsp;doloremque ex&nbsp;exercitationem numquam optio quae sapiente soluta suscipit.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </main>
+        <footer class="footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 col-sm-8 col-md-6 form-block">
+                        <form id="form" class="form">
+                            <div class="contacts-form">
+                                <div class="contacts-form-text">
+                                    <h3 class="contacts-form-h">
+                                        Your message has been sent
+                                    </h3>
+                                    <p class="contacts-form-p">
+                                        Our specialists will contact you shortly
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="form-send">
+                                <label for="name" class="form-label">
+                                    <span class="form-text">name:</span>
+                                    <span class="form-text-hover">
+                                        Antonio Banderas
+                                    </span>
+                                    <input type="text" id="name" name="name" class="input valid-string" placeholder="full name">
+                                </label>
+                                <label for="phone" class="form-label bottom">
+                                    <span class="form-text">phone:</span>
+                                    <span class="form-text-hover">
+                                        + 44 XXX XXX XX XX
+                                    </span>
+                                    <input type="text" id="phone" name="phone" class="input phone valid-string" placeholder="+44">
+                                </label>
+                                <p class="price name">price</p>
+                                <p class="price old">
+                                    €98
+                                </p>
+                                <p class="price">
+                                    €39
+                                </p>
+                                <p class="timer">the offer is limited!</p>
+                                <div class="timer-block">
+                                    <ul class="timer-block-ul">
+                                        <li class="timer-block-ul-li">2</li>
+                                        <li class="timer-block-ul-li">3</li>
+                                        <li class="timer-points">
+                                            <div class="timer-point"></div>
+                                            <div class="timer-point"></div>
+                                        </li>
+                                        <li class="timer-block-ul-li">2</li>
+                                        <li class="timer-block-ul-li">1</li>
+                                        <li class="timer-points">
+                                            <div class="timer-point"></div>
+                                            <div class="timer-point"></div>
+                                        </li>
+                                        <li class="timer-block-ul-li">3</li>
+                                        <li class="timer-block-ul-li">9</li>
+                                    </ul>
+                                </div>
+                                <button class="button submit">order</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
+</div>
+<script src="js/swiper-bundle.min.js?v=1"></script>
+<script src="js/swiper-slider.js?v=1"></script>
+<script src="js/jquery.maskedinput.min.js"></script>
+<script src="js/other-script.js?v=1"></script>
+<script src="js/timer.js?v=1"></script>
+</body>
+</html>
