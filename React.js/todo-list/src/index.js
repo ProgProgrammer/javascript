5e8326f9e6caac3aca53a7ebd1c6145c40f7b3bd
ProgProgrammer <progr.programmer@yandex.ru>
2020-08-05 21:21:15 +0400

    Todo Application: added imports "add-header", "search-panel" and "todo-list" modules; changed "App" function.

--- React.js/todo-list/src/index.js
+++ React.js/todo-list/src/index.js
@@ -1,5 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const el = <h1>Hello World</h1>;
-ReactDOM.render(el, document.getElementById('root'));
+import AppHeader from './components/app-header';
+import SearchPanel from './components/search-panel';
+import TodoList from './components/todo-list';
+
+const App = () =>
+{
+    const isLoggedIn = true;
+    const welcomeBox = "Welcome Back";
+    const loginBox = "Login, please.";
+    const todoData =
+    [
+        { label: 'Drink coffee', important: false },
+        { label: 'Make Awesome App', important: true },
+        { label: 'Have a lunch', important: false }
+    ]
+
+    return(
+        <div>
+            <p>{ new Date().toString() }</p>
+            { isLoggedIn ? welcomeBox : loginBox }
+            <AppHeader />
+            <SearchPanel />
+            <TodoList todos={ todoData } />
+        </div>
+    );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
