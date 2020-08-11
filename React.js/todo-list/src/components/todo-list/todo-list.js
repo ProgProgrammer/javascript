d63223326d7b51cdd474772ba89764b9069ed5dc
ProgProgrammer <progr.programmer@yandex.ru>
2020-08-12 00:15:32 +0400

    Todo Application: changed path to folder with "TodoListItem" function.

--- React.js/todo-list/src/components/todo-list/todo-list.js
+++ React.js/todo-list/src/components/todo-list/todo-list.js
@@ -1,11 +1,10 @@
 import React from 'react';
 
-import TodoListItem from './todo-list-item';
+import TodoListItem from '../todo-list-item';
 import "./todo-list.css";
 
 const TodoList = ({ todos }) =>
 {
-
     const elements = todos.map((item) =>
     {
         const { id, ...itemProps } = item;
