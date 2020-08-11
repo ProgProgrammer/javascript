24a7cc8c132ea6520c8c8af5b85e7761822383f2
ProgProgrammer <progr.programmer@yandex.ru>
2020-08-11 16:12:58 +0400

    Todo Application: added importing "todo-list-item.css" file in "todo-list-item.js" file.

--- React.js/todo-list/src/components/todo-list-item.js
+++ React.js/todo-list/src/components/todo-list-item.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import "./todo-list-item.css";
 
 const TodoListItem = ( { label, important = false } ) =>
 {
@@ -7,7 +8,9 @@ const TodoListItem = ( { label, important = false } ) =>
         color: important ? 'tomato' : 'black'
     };
 
-    return <span style={ style }>{ label }</span>;
+    return (
+        <span className="todo-list-item" style={ style }>{ label }</span>
+    );
 }
 
 export default TodoListItem;
