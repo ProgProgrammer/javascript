73283bc858d3d766319e7bcaffb4347aa8ebf8ac
ProgProgrammer <progr.programmer@yandex.ru>
2020-08-13 23:17:28 +0400

    Todo Application: removed "App" function in "app.js" file; added "App" class in "app.js" file; added "state" object in "app.js" file; added "deleteItem" methode; added "render" methode in "app.js" file.

--- React.js/todo-list/src/components/app/app.js
+++ React.js/todo-list/src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 
 import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
@@ -7,30 +7,54 @@ import TodoList from '../todo-list';
 
 import "./app.css";
 
-const App = () =>
+export default class App extends Component
 {
-    const isLoggedIn = true;
-    const welcomeBox = "Welcome Back";
-    const loginBox = "Login, please.";
-    const todoData =
+    isLoggedIn = true;
+    welcomeBox = "Welcome Back";
+    loginBox = "Login, please.";
+
+    state =
+    {
+        todoData:
         [
-            { label: 'Drink coffee', important: false, id: 'nb' },
-            { label: 'Make Awesome App', important: true, id: 'nm' },
-            { label: 'Have a lunch', important: false, id: 'nh' }
+            { label: 'Drink coffee', id: 'nb' },
+            { label: 'Make Awesome App', id: 'nm' },
+            { label: 'Have a lunch', id: 'nh' }
         ]
+    };
 
-    return(
-        <div className="todo-app">
-            <p>{ new Date().toString() }</p>
-            { isLoggedIn ? welcomeBox : loginBox }
-            <AppHeader toDo={1} done={3} />
-            <div className="top-panel d-flex">
-                <SearchPanel />
-                <ItemStatusFilter />
+    deleteItem = (id) =>
+    {
+        this.setState(({ todoData }) =>
+        {
+            const idx = todoData.findIndex((el) => el.id === id);
+
+            const newArray = [
+                ...todoData.slice(0, idx),
+                ...todoData.slice(idx + 1)
+            ];
+
+            return {
+                todoData: newArray
+            };
+        });
+    }
+
+    render()
+    {
+        return(
+            <div className="todo-app">
+                <p>{ new Date().toString() }</p>
+                { this.isLoggedIn ? this.welcomeBox : this.loginBox }
+                <AppHeader toDo={1} done={3} />
+                <div className="top-panel d-flex">
+                    <SearchPanel />
+                    <ItemStatusFilter />
+                </div>
+                <TodoList
+                    todos={ this.state.todoData }
+                    onDeleted={  this.deleteItem }/>
             </div>
-            <TodoList todos={ todoData } />
-        </div>
-    );
+        );
+    }
 }
-
-export default App;
