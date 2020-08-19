069b1f91df56e99a78e50601613f3c33c30e8699
ProgProgrammer <progr.programmer@yandex.ru>
2020-08-19 23:02:39 +0400

    Todo Application: added "form" tag in "item-add-form.js" file; added "state" object in "item-add-form.js" file; added "onLabelChange" and "onSubmit" methods in "item-add-form.js" file.

--- React.js/todo-list/src/components/item-add-form/item-add-form.js
+++ React.js/todo-list/src/components/item-add-form/item-add-form.js
@@ -4,14 +4,37 @@ import "./item-add-form.css";
 
 export default class ItemPropsForm extends Component
 {
+    state =
+    {
+        label: ''
+    };
+
+    onLabelChange = (event) =>
+    {
+        this.setState(
+        {
+            label: event.target.value
+        });
+    }
+
+    onSubmit = (event) =>
+    {
+        event.preventDefault();
+        this.props.onAdded(this.state.label);
+        const inputText = document.querySelector(".form-control");
+        inputText.value = '';
+    }
+
     render()
     {
         return(
-            <div className="block-button">
-                <button className="add-button"
-                        onClick={ () => this.props.onAdded("Hello, world!") }
-                >Add item</button>
-            </div>
+            <form className="block-button item-add-form d-flex"
+                  onSubmit={ this.onSubmit } >
+                <input type="text" className="form-control"
+                        onChange={this.onLabelChange}
+                        placeholder="What needs to be done"/>
+                <button className="add-button">Add item</button>
+            </form>
         );
     }
 }
