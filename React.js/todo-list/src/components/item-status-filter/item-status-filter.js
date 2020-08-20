a3a2c702cb747e6c2b128d24b98eeb427e350c31
ProgProgrammer <progr.programmer@yandex.ru>
2020-08-20 21:21:12 +0400

    Todo Application: added "onActiveDone" methode and "onClick" events to "item-status-filter.js" file

--- React.js/todo-list/src/components/item-status-filter/item-status-filter.js
+++ React.js/todo-list/src/components/item-status-filter/item-status-filter.js
@@ -4,13 +4,21 @@ import "./item-status-filter.css";
 
 export default class ItemStatusFilter extends Component
 {
+    onActiveDone(value)
+    {
+        this.props.onActiveDone(value);
+    }
+
     render()
     {
         return(
             <div className="btn-group">
-                <button className="btn btn-info">All</button>
-                <button className="btn btn-outline-secondary">Active</button>
-                <button className="btn btn-outline-secondary">Done</button>
+                <button className="btn btn-info"
+                        onClick={ () => this.onActiveDone('all') }>All</button>
+                <button className="btn btn-outline-secondary"
+                        onClick={ () => this.onActiveDone('active') }>Active</button>
+                <button className="btn btn-outline-secondary"
+                        onClick={ () => this.onActiveDone('done') }>Done</button>
             </div>
         );
     }
