0dca75eb58134b15d1b38e0ca0e799ae1aabc7af
ProgProgrammer <progr.programmer@yandex.ru>
2020-08-27 22:10:31 +0400

    StarDB: removed "getResource" function to "index.js" file; created "SwapiService" class to "index.js" file; added "getResource", "getAllPeople", "getPerson", "getAllPlanets", "getPlanet", "getAllStarShips", "getStarShip" methodes in "SwapiService" class to "index.js" file.

--- React.js/star-db/src/index.js
+++ React.js/star-db/src/index.js
@@ -1,21 +1,102 @@
 
-const getResource = async (url) =>
+class SwapiService
 {
-    const res = await fetch(url);
-    if (!res.ok)
+    _apiBase = 'https://swapi.dev/api';
+
+    async getResource(url)
     {
-        throw new Error(`could not fetch ${url}, received ${res.status}`);
+        const res = await fetch(`${this._apiBase}${url}`);
+        if (!res.ok)
+        {
+            throw new Error(`could not fetch ${url}, received ${res.status}`);
+        }
+        const body = await res.json();
+        return body;
     }
-    const body = await res.json();
-    return body;
+    
+    async getAllPeople()
+    {
+        const res = await this.getResource(`/people/`);
+        return res.results;
+    }
+
+    getPerson(id)
+    {
+        return this.getResource(`/people/${id}`);
+    }
+
+    async getAllPlanets()
+    {
+        const res = await this.getResource(`/planets/`);
+        return res.results;
+    }
+
+    getPlanet(id)
+    {
+        return this.getResource(`/planets/${id}`);
+    }
+
+    async getAllStarShips()
+    {
+        const res = await this.getResource(`/starships/`);
+        return res.results;
+    }
+
+    getStarShip(id)
+    {
+        return this.getResource(`/starships/${id}`);
+    }
 }
 
-getResource('https://swapi.dev/api/people/1/')
-    .then((body) =>
+const swapi = new SwapiService();
+
+swapi.getAllPeople()
+    .then((people) =>
     {
-        console.log(body);
-    })
-    .catch((err) =>
+        console.log("");
+        console.log(`All people:`);
+        people.forEach((item) =>
+        {
+            console.log(item.name);
+        });
+    });
+
+swapi.getPerson(3)
+    .then((person) =>
     {
-        console.log('Could not fetch:', err);
+        console.log(`${person.name} - person.`);
+    });
+
+swapi.getAllPlanets()
+    .then((people) =>
+    {
+        console.log("");
+        console.log(`All planets:`);
+        people.forEach((item) =>
+        {
+            console.log(item.name);
+        });
+    });
+
+swapi.getPlanet(3)
+    .then((person) =>
+    {
+        console.log(`${person.name} - planet.`);
+    });
+
+swapi.getAllStarShips()
+    .then((people) =>
+    {
+        console.log("");
+        console.log(`All StarShips:`);
+        people.forEach((item) =>
+        {
+            console.log(item.name);
+        });
+    });
+
+swapi.getStarShip(3)
+    .then((person) =>
+    {
+        console.log(`${person.name} - StarShip.`);
     });
