56954c742752d7d084a86a64909accda94d412c6
ProgProgrammer <progr.programmer@yandex.ru>
2020-08-28 18:02:44 +0400

    StarDB: changed "index.js" file in src folder.

--- React.js/star-db/src/index.js
+++ React.js/star-db/src/index.js
@@ -1,52 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
 
-class SwapiService
-{
-    _apiBase = 'https://swapi.dev/api';
+ReactDOM.render(<App />,
+    document.getElementById("root"));
 
-    async getResource(url)
-    {
-        const res = await fetch(`${this._apiBase}${url}`);
-        if (!res.ok)
-        {
-            throw new Error(`could not fetch ${url}, received ${res.status}`);
-        }
-        const body = await res.json();
-        return body;
-    }
-    
-    async getAllPeople()
-    {
-        const res = await this.getResource(`/people/`);
-        return res.results;
-    }
-
-    getPerson(id)
-    {
-        return this.getResource(`/people/${id}`);
-    }
-
-    async getAllPlanets()
-    {
-        const res = await this.getResource(`/planets/`);
-        return res.results;
-    }
-
-    getPlanet(id)
-    {
-        return this.getResource(`/planets/${id}`);
-    }
-
-    async getAllStarShips()
-    {
-        const res = await this.getResource(`/starships/`);
-        return res.results;
-    }
-
-    getStarShip(id)
-    {
-        return this.getResource(`/starships/${id}`);
-    }
-}
+/*import { SwapiService } from "services/swapi-service.js";
 
 const swapi = new SwapiService();
 
@@ -99,4 +58,4 @@ swapi.getStarShip(3)
     .then((person) =>
     {
         console.log(`${person.name} - StarShip.`);
-    });
+    });*/
