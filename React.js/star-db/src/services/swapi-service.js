44f9a6b4f40d48053175da3e34b09ea2d1af6abc
ProgProgrammer <progr.programmer@yandex.ru>
2020-08-28 18:14:20 +0400

    StarDB: added "services" folder with "swapi-service.js" file.

---
+++ React.js/star-db/src/services/swapi-service.js
@@ -0,0 +1,49 @@
+
+export default class SwapiService
+{
+    _apiBase = 'https://swapi.dev/api';
+
+    async getResource(url)
+    {
+        const res = await fetch(`${this._apiBase}${url}`);
+        if (!res.ok)
+        {
+            throw new Error(`could not fetch ${url}, received ${res.status}`);
+        }
+        const body = await res.json();
+        return body;
+    }
+
+    async getAllPeople()
+    {
+        const res = await this.getResource(`/people/`);
+        return res.results;
+    }
+
+    getPerson(id)
+    {
+        return this.getResource(`/people/${id}`);
+    }
+
+    async getAllPlanets()
+    {
+        const res = await this.getResource(`/planets/`);
+        return res.results;
+    }
+
+    getPlanet(id)
+    {
+        return this.getResource(`/planets/${id}`);
+    }
+
+    async getAllStarShips()
+    {
+        const res = await this.getResource(`/starships/`);
+        return res.results;
+    }
+
+    getStarShip(id)
+    {
+        return this.getResource(`/starships/${id}`);
+    }
+}
