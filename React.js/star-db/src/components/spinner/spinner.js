7072afe3d2baabe499fd02f8693a776da3337e6c
ProgProgrammer <progr.programmer@yandex.ru>
2020-08-29 17:54:29 +0400

    StarDB: created "spinner" component; created "spinner" folder with "index.js", "spinner.css" and "spinner.js" files.

---
+++ React.js/star-db/src/components/spinner/spinner.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import './spinner.css';
+
+const Spinner = () =>
+{
+    return(
+        <div className="content-center">
+            <div className="loadingio-spinner-double-ring-h123vgvid9">
+                <div className="ldio-dtfqobt7msm">
+                    <div></div>
+                    <div></div>
+                    <div>
+                        <div></div>
+                    </div>
+                    <div>
+                        <div></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Spinner;
